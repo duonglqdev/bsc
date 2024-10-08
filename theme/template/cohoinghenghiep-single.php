5ac7da27184fb40724d489c21a56f8ccc7b85ed8
duonglqdev <duonglq.dev@gmail.com>
2024-10-08 23:15:11 +0700

    add single cohoinghenhiep #1

---
+++ theme/template/cohoinghenghiep-single.php
@@ -0,0 +1,281 @@
+<?php
+
+/**
+Template Name: Chi tiết tuyển dụng
+ */
+
+get_header();
+?>
+<main>
+	<?php get_template_part( 'components/page-banner' ) ?>
+	<section class="lg:lg:my-[100px] my-10">
+		<div class="container">
+			<div class="grid lg:grid-cols-3">
+				<div class="lg:col-span-2">
+					<h1 class="lg:text-[32px] text-2xl font-bold mb-10">
+						[Ban Tài Chính BSC] Chuyên viên cao cấp/Chuyên gia Phân Tích Kinh Doanh
+					</h1>
+				</div>
+			</div>
+			<div class="grid md:grid-cols-4 lg:gap-[70px] gap-10">
+				<div class="md:col-span-1 col-span-full">
+					<div class="bg-[#F5FCFF] py-5 px-6 rounded-[10px]">
+						<h3 class="text-primary-300 uppercase text-xl font-bold">
+							THÔNG TIN CHUNG
+						</h3>
+						<div class="mt-[12px]">
+							<div
+								class="[&:not(:last-child)]:mb-[12px] [&:not(:last-child)]:pb-[12px] [&:not(:last-child)]:border-b border-[#C6C6C6] font-Helvetica">
+								<p class="text-black mb-2 text-xs">
+									Cấp bậc
+								</p>
+								<p class="font-bold text-xs">
+									Kinh doanh Nguồn vốn và Trái phiếu
+								</p>
+							</div>
+							<div
+								class="[&:not(:last-child)]:mb-[12px] [&:not(:last-child)]:pb-[12px] [&:not(:last-child)]:border-b border-[#C6C6C6] font-Helvetica">
+								<p class="text-black mb-2 text-xs">
+									Nghành nghề
+								</p>
+								<p class="font-bold text-xs">
+									Chuyên viên kinh doanh
+								</p>
+							</div>
+							<div
+								class="[&:not(:last-child)]:mb-[12px] [&:not(:last-child)]:pb-[12px] [&:not(:last-child)]:border-b border-[#C6C6C6] font-Helvetica">
+								<p class="text-black mb-2 text-xs">
+									Mã vị trí
+								</p>
+								<p class="font-bold text-xs">
+									J24.01
+								</p>
+							</div>
+							<div
+								class="[&:not(:last-child)]:mb-[12px] [&:not(:last-child)]:pb-[12px] [&:not(:last-child)]:border-b border-[#C6C6C6] font-Helvetica">
+								<p class="text-black mb-2 text-xs">
+									Địa điểm làm việc
+								</p>
+								<p class="font-bold text-xs">
+									Hà Nội
+								</p>
+							</div>
+							<div
+								class="[&:not(:last-child)]:mb-[12px] [&:not(:last-child)]:pb-[12px] [&:not(:last-child)]:border-b border-[#C6C6C6] font-Helvetica">
+								<p class="text-black mb-2 text-xs">
+									Cấp bậc
+								</p>
+								<p class="font-bold text-xs">
+									Chuyên viên
+								</p>
+							</div>
+							<div
+								class="[&:not(:last-child)]:mb-[12px] [&:not(:last-child)]:pb-[12px] [&:not(:last-child)]:border-b border-[#C6C6C6] font-Helvetica">
+								<p class="text-black mb-2 text-xs">
+									Mức lương
+								</p>
+								<p class="font-bold text-xs">
+									Thỏa thuận
+								</p>
+							</div>
+							<div
+								class="[&:not(:last-child)]:mb-[12px] [&:not(:last-child)]:pb-[12px] [&:not(:last-child)]:border-b border-[#C6C6C6] font-Helvetica">
+								<p class="text-black mb-2 text-xs">
+									Hạn nộp hồ sơ
+								</p>
+								<p class="font-bold text-xs">
+									26/09/2025
+								</p>
+							</div>
+						</div>
+						<div class="mt-[12px]">
+							<button
+								class="bg-yellow-100 text-black after:bg-green hover:text-white inline-block px-6 py-3 rounded-xl font-semibold relative transition-all duration-500 after:absolute after:h-full after:w-0 after:top-0 after:left-0 after:transition-all after:duration-500 after:opacity-0 after:rounded-xl hover:after:w-full hover:after:opacity-100 w-full h-full">
+								<span class="block relative z-10">
+									<?php _e( 'Ứng tuyển ngay', 'bsc' ) ?>
+								</span>
+							</button>
+
+						</div>
+					</div>
+				</div>
+				<div class="md:col-span-3 col-span-full space-y-[30px]">
+					<div class="content_item">
+						<h2
+							class="font-bold lg:text-2xl text-lg mb-5 text-primary-300 font-Helvetica">
+							Yêu cầu
+						</h2>
+						<div class="the_content">
+							Tốt nghiệp Đại học trở lên thuộc chuyên ngành kinh tế, tài chính, ngân
+							hàng, đầu tư hoặc các chuyên ngành khác có liên quan.
+							Ưu tiên Ứng viên có kinh nghiệm làm việc trong lĩnh vực chuyên môn có
+							liên quan.
+							Ưu tiên Ứng viên có chứng chỉ hành nghề chứng khoán và/hoặc chứng chỉ
+							CFA, FRM.
+							Tiếng Anh lưu loát; Thành thạo tin học văn phòng.
+							Thái độ làm việc tích cực, tư duy logic, ý thức tuân thủ và trách nhiệm
+							cao trong công việc.
+							Có kỹ năng giao tiếp, thuyết trình tốt.
+							Năng động, sáng tạo, chủ động và chịu được áp lực công việc cao.
+						</div>
+					</div>
+					<div class="content_item">
+						<h2
+							class="font-bold lg:text-2xl text-lg mb-5 text-primary-300 font-Helvetica">
+							Mô tả công việc
+						</h2>
+						<div class="the_content">
+							Xây dựng kế hoạch, đề xuất cơ hội đầu tư và trực tiếp xử lý các giao
+							dịch phát sinh.
+							Tìm kiếm, chăm sóc khách hàng và thực hiện giao dịch phục vụ khách hàng
+							theo nhu cầu.
+							Thực hiện giao dịch môi giới đặt lệnh và môi giới đấu thầu Trái phiếu
+							Chính phủ và Trái phiếu Chính phủ bảo lãnh cho khách hàng và các công
+							tác chăm sóc khách hàng khác.
+							Thực hiện báo cáo nghiên cứu, cập nhật thị trường và các báo cáo nội bộ
+							liên quan.
+						</div>
+					</div>
+					<div class="content_item">
+						<h2
+							class="font-bold lg:text-2xl text-lg mb-5 text-primary-300 font-Helvetica">
+							Quyền lợi được hưởng
+						</h2>
+						<div class="the_content">
+							Mức thu nhập cạnh tranh dựa trên năng lực, kinh nghiệm và hiệu suất công
+							việc.
+							Được thưởng theo hiệu quả kinh doanh của công ty, thưởng trong các dịp
+							lễ Tết.
+							Được hưởng các chế độ BHXH, BHYT, Bảo hiểm sức khỏe, khám bệnh định kỳ
+							cho cán bộ.
+							Chế độ du lịch hàng năm cho cán bộ.Có cơ hội làm việc trong môi trường
+							chuyên nghiệp, tạo cơ hội thăng tiến trong công việc.
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+    <section class="lg:lg:my-[100px] my-10">
+        <div class="container">
+            <h3 class="font-bold text-primary-300 lg:text-[32px] text-2xl">
+            CÁC VỊ TRÍ TUYỂN DỤNG KHÁC
+            </h3>
+            <div class="mt-10">
+				<?php
+				for ( $i = 0; $i < 2; $i++ )
+				{
+					?>
+
+					<div
+						class="job_item grid lg:grid-cols-9 gap-5 font-Helvetica mb-[30px] pb-[30px] border-b border-[#C3C3C3]">
+						<div class="col-span-3 job_title">
+							<p class="mb-4 text-gray-100 font-medium">Vị trí ứng tuyển</p>
+							<h4
+								class="font-bold text-lg transition-all duration-500 hover:text-primary-300">
+								<a href="" class="line-clamp-3">
+									[Ban Tài Chính BSC] Chuyên viên cao cấp/Chuyên gia Phân Tích Kinh
+									Doanh
+								</a>
+							</h4>
+						</div>
+						<div class="col-span-1">
+							<p class="mb-4 text-gray-100 font-medium">Kinh nghiệm</p>
+							<p class="font-bold text-lg job_exp">
+								3 năm
+							</p>
+						</div>
+						<div class="col-span-1 ">
+							<p class="mb-4 text-gray-100 font-medium">Nơi làm việc</p>
+							<p class="font-bold text-lg job_location">
+								Hà Nội
+							</p>
+						</div>
+						<div class="col-span-1">
+							<p class="mb-4 text-gray-100 font-medium">Số lượng tuyển</p>
+							<p class="font-bold text-lg job_number">
+								2 người
+							</p>
+						</div>
+						<div class="col-span-1 ">
+							<p class="mb-4 text-gray-100 font-medium">Hạn nộp hồ sơ</p>
+							<p class="font-bold text-lg job_date">
+								05/10/2026
+							</p>
+						</div>
+						<div class="col-span-1 job_status m-auto">
+							<div
+								class="urgent inline-block rounded-full px-4 py-2 font-bold text-xs  [&:not(.urgent)]:text-[#30D158] [&:not(.urgent)]:bg-[#B5F8C5] text-[#F9162A] bg-[#FFB2B9]">
+								Tuyển gấp
+							</div>
+						</div>
+						<div class="col-span-1 m-auto">
+							<a href=""
+								class="text-green font-bold inline-flex gap-x-3 items-center transition-all duration-500 hover:text-primary-300 text-xs">
+
+								<?php _e( 'Xem chi tiết', 'bsc' ) ?>
+								<?php echo svg( 'arrow-btn', '12', '12' ) ?>
+							</a>
+						</div>
+					</div>
+					<div
+						class="job_item grid lg:grid-cols-9 gap-5 font-Helvetica mb-[30px] pb-[30px] border-b border-[#C3C3C3]">
+						<div class="col-span-3 job_title">
+							<p class="mb-4 text-gray-100 font-medium">Vị trí ứng tuyển</p>
+							<h4
+								class="font-bold text-lg transition-all duration-500 hover:text-primary-300">
+								<a href="" class="line-clamp-3">
+									[Ban Tài Chính BSC] Chuyên viên cao cấp/Chuyên gia Phân Tích Kinh
+									Doanh
+								</a>
+							</h4>
+						</div>
+						<div class="col-span-1">
+							<p class="mb-4 text-gray-100 font-medium">Kinh nghiệm</p>
+							<p class="font-bold text-lg job_exp">
+								3 năm
+							</p>
+						</div>
+						<div class="col-span-1 ">
+							<p class="mb-4 text-gray-100 font-medium">Nơi làm việc</p>
+							<p class="font-bold text-lg job_location">
+								Hà Nội
+							</p>
+						</div>
+						<div class="col-span-1">
+							<p class="mb-4 text-gray-100 font-medium">Số lượng tuyển</p>
+							<p class="font-bold text-lg job_number">
+								2 người
+							</p>
+						</div>
+						<div class="col-span-1 ">
+							<p class="mb-4 text-gray-100 font-medium">Hạn nộp hồ sơ</p>
+							<p class="font-bold text-lg job_date">
+								05/10/2026
+							</p>
+						</div>
+						<div class="col-span-1 job_status m-auto">
+							<div
+								class="inline-block rounded-full px-4 py-2 font-bold text-xs  [&:not(.urgent)]:text-[#30D158] [&:not(.urgent)]:bg-[#B5F8C5] text-[#F9162A] bg-[#FFB2B9]">
+								Đang tuyển
+							</div>
+						</div>
+						<div class="col-span-1 m-auto">
+							<a href=""
+								class="text-green font-bold inline-flex gap-x-3 items-center transition-all duration-500 hover:text-primary-300 text-xs">
+
+								<?php _e( 'Xem chi tiết', 'bsc' ) ?>
+								<?php echo svg( 'arrow-btn', '12', '12' ) ?>
+							</a>
+						</div>
+					</div>
+					<?php
+				}
+				?>
+			</div>
+        </div>
+    </section>
+</main>
+<?php
+get_footer();
