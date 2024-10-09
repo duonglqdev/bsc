f43516d9a3d06fa14c2b52266a4c863c185a2aa7
duonglq.dev <duonglq.dev@gmail.com>
2024-10-09 08:57:02 +0700

    update fix bug

--- theme/taxonomy-danh-muc-bao-cao.php
+++ theme/taxonomy-danh-muc-bao-cao.php
@@ -31,7 +31,7 @@ get_header();
                                 ?>
                                     <li class="<?php echo esc_attr($active_class); ?>">
                                         <a href="<?php echo get_term_link($term); ?>"
-                                            class="flex items-center gap-4 md:text-lg font-bold <?php echo esc_attr($active_class); ?> [&:not(.active)]:text-black text-white transition-all relative py-[12px] px-5 before:w-2 before:h-2 before:rounded-[2px] [&:not(.active)]:before:bg-[#051D36] [&:not(.active)]:before:bg-opacity-50 before:bg-white before:bg-opacity-100 bg-primary-300 [&:not(.active)]:bg-white hover:!bg-primary-300 hover:!text-white hover:before:!bg-white hover:before:!bg-opacity-100 rounded-tr-xl rounded-br-xl">
+                                            class="flex items-center gap-4 md:text-lg font-bold <?php echo esc_attr($active_class); ?> [&:not(.active)]:text-black text-white relative py-[12px] px-5 before:w-2 before:h-2 before:rounded-[2px] [&:not(.active)]:before:bg-[#051D36] [&:not(.active)]:before:bg-opacity-50 before:bg-white before:bg-opacity-100 bg-primary-300 [&:not(.active)]:bg-white hover:!bg-primary-300 hover:!text-white hover:before:!bg-white hover:before:!bg-opacity-100 rounded-tr-xl rounded-br-xl">
                                             <?php echo esc_html($term->name); ?>
                                         </a>
                                         <?php
