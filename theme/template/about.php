e8a1bcad4a7ce548b498b0980452ffd2a1d817ca
duonglq.dev <duonglq.dev@gmail.com>
2024-10-25 16:28:19 +0700

    fix modal

--- theme/template/about.php
+++ theme/template/about.php
@@ -507,7 +507,7 @@ get_header();
 
 	</section>
 	<div id="leader-modal" tabindex="-1" aria-hidden="true"
-		class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full bg-black bg-opacity-20">
+		class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-full max-h-full bg-black bg-opacity-20">
 		<div class="relative p-4 w-full max-w-2xl lg:max-w-[1094px] max-h-full">
 			<div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
 
