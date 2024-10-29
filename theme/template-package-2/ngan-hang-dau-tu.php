b4e3924970a5ae86bf818a177611957422c4e3ec
duonglq.dev <duonglq.dev@gmail.com>
2024-10-29 17:39:30 +0700

    ngan-hang-dau-tu #1

--- theme/template-package-2/ngan-hang-dau-tu.php
+++ theme/template-package-2/ngan-hang-dau-tu.php
@@ -8,6 +8,7 @@ get_header();
 ?>
 <main>
 	<?php get_template_part( 'components/page-banner' ) ?>
+	<!-- Tab nav -->
 	<section class="2xl:py-4 py-3 bg-primary-50">
 		<div class="container">
 			<ul class="customtab-nav flex justify-between">
@@ -38,77 +39,215 @@ get_header();
 			</ul>
 		</div>
 	</section>
-	<section class="xl:my-[100px] md:my-20 my-10">
-		<div class="container">
-			<div id="investment-bank-tab-content">
-				<div class="block tab-content" id="ecm-tab">
-					<div class="lg:grid lg:grid-cols-2 xl:gap-0 gap-10">
-						<div class="col-span-1 xl:pr-[42px]">
-							<h2 class="heading-title mb-4">
-								DỊCH VỤ THỊ TRƯỜNG VỐN (ECM)
-							</h2>
-							<p class="mb-4 font-bold">BSC cung cấp đa dạng các dịch vụ thị trường
-								vốn
-								bao gồm:</p>
-							<ul class="flex-1 space-y-4 list-icon">
-								<li class="font-semibold list-icon-item">
-									Tư vấn cổ phần hóa
-								</li>
-								<li class="font-semibold list-icon-item">
-									Tư vấn chào bán cổ phần lần đầu ra công chúng (IPO)
-								</li>
-								<li class="font-semibold list-icon-item">
-									Tư vấn phát hành riêng lẻ cổ phiếu
-								</li>
-								<li class="font-semibold list-icon-item">
-									Tư vấn đăng ký niêm yết cổ phiểu
-								</li>
-							</ul>
-							<div class="font-bold mt-4 text-justify">
-							Với nhiều năm kinh nghiệm trên thị trường, đội ngũ chuyên gia của BSC sẽ hỗ trợ doanh nghiệp một cách toàn diện thông qua hoạt động cung cấp dịch vụ trọn gói, từ tư vấn xây dựng phương án, cấu trúc giao dịch, chuẩn bị hồ sơ, cho đến tư vấn đàm phán điều kiện điều khoản, và các công tác hỗ trợ sau giao dịch.
-							</div>
-							<div class="mt-8">
-								<a href=""
-									class="btn-base-yellow px-6 py-4 inline-flex items-center gap-x-3">
-									<?php echo svg( 'arrow-btn', '16', '16' ) ?>
-									Liên hệ tư vấn
-								</a>
-							</div>
+	<!-- Tab content -->
+	<section class="block tab-content" id="ecm-tab">
+		<div class="xl:my-[100px] md:my-20 my-10">
+			<div class="container">
+				<div class="lg:grid lg:grid-cols-2 xl:gap-0 gap-10">
+					<div class="col-span-1 xl:pr-[42px]">
+						<h2 class="heading-title mb-4">
+							DỊCH VỤ THỊ TRƯỜNG VỐN (ECM)
+						</h2>
+						<p class="mb-4 font-bold">BSC cung cấp đa dạng các dịch vụ thị trường
+							vốn
+							bao gồm:</p>
+						<ul class="flex-1 space-y-4 list-icon">
+							<li class="font-semibold list-icon-item">
+								Tư vấn cổ phần hóa
+							</li>
+							<li class="font-semibold list-icon-item">
+								Tư vấn chào bán cổ phần lần đầu ra công chúng (IPO)
+							</li>
+							<li class="font-semibold list-icon-item">
+								Tư vấn phát hành riêng lẻ cổ phiếu
+							</li>
+							<li class="font-semibold list-icon-item">
+								Tư vấn đăng ký niêm yết cổ phiểu
+							</li>
+						</ul>
+						<div class="font-bold mt-4 text-justify">
+							Với nhiều năm kinh nghiệm trên thị trường, đội ngũ chuyên gia của
+							BSC sẽ hỗ trợ doanh nghiệp một cách toàn diện thông qua hoạt động
+							cung cấp dịch vụ trọn gói, từ tư vấn xây dựng phương án, cấu trúc
+							giao dịch, chuẩn bị hồ sơ, cho đến tư vấn đàm phán điều kiện điều
+							khoản, và các công tác hỗ trợ sau giao dịch.
 						</div>
-						<div class="col-span-1 xl:pl-[22px]">
-							<div class="relative overflow-hidden pt-[65.743%] w-full rounded-2xl">
-								<img src="<?php echo get_stylesheet_directory_uri()?>/assets/images/ba1.jpg" alt="" class="absolute w-full h-full inset-0 object-cover transition-all duration-500 hover:scale-105">
-							</div>
+						<div class="mt-8">
+							<a href=""
+								class="btn-base-yellow px-6 py-4 inline-flex items-center gap-x-3">
+								<?php echo svg( 'arrow-btn', '16', '16' ) ?>
+								Liên hệ tư vấn
+							</a>
+						</div>
+					</div>
+					<div class="col-span-1 xl:pl-[22px]">
+						<div class="relative overflow-hidden pt-[65.743%] w-full rounded-2xl">
+							<img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/ba1.jpg"
+								alt=""
+								class="absolute w-full h-full inset-0 object-cover transition-all duration-500 hover:scale-105">
 						</div>
 					</div>
 				</div>
-				<div class="hidden tab-content" id="dcm-tab">
-					<p class="text-sm text-gray-500 dark:text-gray-400">This is some placeholder
-						content the <strong
-							class="font-medium text-gray-800 dark:text-white">Dashboard tab's
-							associated content</strong>. Clicking another tab will toggle the
-						visibility of this one for the next. The tab JavaScript swaps classes to
-						control the content visibility and styling.</p>
-				</div>
-				<div class="hidden tab-content" id="tvtc-tab">
-					<p class="text-sm text-gray-500 dark:text-gray-400">This is some placeholder
-						content the <strong
-							class="font-medium text-gray-800 dark:text-white">Settings tab's
-							associated content</strong>. Clicking another tab will toggle the
-						visibility of this one for the next. The tab JavaScript swaps classes to
-						control the content visibility and styling.</p>
-				</div>
-				<div class="hidden tab-content" id="ma-tab">
-					<p class="text-sm text-gray-500 dark:text-gray-400">This is some placeholder
-						content the <strong
-							class="font-medium text-gray-800 dark:text-white">Contacts tab's
-							associated content</strong>. Clicking another tab will toggle the
-						visibility of this one for the next. The tab JavaScript swaps classes to
-						control the content visibility and styling.</p>
+
+			</div>
+		</div>
+	</section>
+
+	<section class="hidden tab-content" id="dcm-tab">
+		<div class="xl:my-[100px] md:my-20 my-10">
+			<div class="container">
+				<div class="lg:grid lg:grid-cols-2 xl:gap-0 gap-10">
+					<div class="col-span-1 xl:pr-[42px]">
+						<h2 class="heading-title mb-4">
+							DỊCH VỤ THỊ TRƯỜNG NỢ (DCM)
+						</h2>
+						<p class="mb-4 font-bold">BSC cung cấp đa dạng các dịch vụ thị trường
+							vốn
+							bao gồm:</p>
+						<ul class="flex-1 space-y-4 list-icon">
+							<li class="font-semibold list-icon-item">
+								Tư vấn cổ phần hóa
+							</li>
+							<li class="font-semibold list-icon-item">
+								Tư vấn chào bán cổ phần lần đầu ra công chúng (IPO)
+							</li>
+							<li class="font-semibold list-icon-item">
+								Tư vấn phát hành riêng lẻ cổ phiếu
+							</li>
+							<li class="font-semibold list-icon-item">
+								Tư vấn đăng ký niêm yết cổ phiểu
+							</li>
+						</ul>
+						<div class="font-bold mt-4 text-justify">
+							Với nhiều năm kinh nghiệm trên thị trường, đội ngũ chuyên gia của
+							BSC sẽ hỗ trợ doanh nghiệp một cách toàn diện thông qua hoạt động
+							cung cấp dịch vụ trọn gói, từ tư vấn xây dựng phương án, cấu trúc
+							giao dịch, chuẩn bị hồ sơ, cho đến tư vấn đàm phán điều kiện điều
+							khoản, và các công tác hỗ trợ sau giao dịch.
+						</div>
+						<div class="mt-8">
+							<a href=""
+								class="btn-base-yellow px-6 py-4 inline-flex items-center gap-x-3">
+								<?php echo svg( 'arrow-btn', '16', '16' ) ?>
+								Liên hệ tư vấn
+							</a>
+						</div>
+					</div>
+					<div class="col-span-1 xl:pl-[22px]">
+						<div class="relative overflow-hidden pt-[65.743%] w-full rounded-2xl">
+							<img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/ba1.jpg"
+								alt=""
+								class="absolute w-full h-full inset-0 object-cover transition-all duration-500 hover:scale-105">
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
 	</section>
+	<section class="hidden tab-content" id="tvtc-tab">
+		<div class="xl:my-[100px] md:my-20 my-10">
+			<div class="container">
+				<div class="lg:grid lg:grid-cols-2 xl:gap-0 gap-10">
+					<div class="col-span-1 xl:pr-[42px]">
+						<h2 class="heading-title mb-4">
+							TƯ VẤN TÀI CHÍNH DOANH NGHIỆP
+						</h2>
+						<p class="mb-4 font-bold">BSC cung cấp đa dạng các dịch vụ thị trường
+							vốn
+							bao gồm:</p>
+						<ul class="flex-1 space-y-4 list-icon">
+							<li class="font-semibold list-icon-item">
+								Tư vấn cổ phần hóa
+							</li>
+							<li class="font-semibold list-icon-item">
+								Tư vấn chào bán cổ phần lần đầu ra công chúng (IPO)
+							</li>
+							<li class="font-semibold list-icon-item">
+								Tư vấn phát hành riêng lẻ cổ phiếu
+							</li>
+							<li class="font-semibold list-icon-item">
+								Tư vấn đăng ký niêm yết cổ phiểu
+							</li>
+						</ul>
+						<div class="font-bold mt-4 text-justify">
+							Với nhiều năm kinh nghiệm trên thị trường, đội ngũ chuyên gia của
+							BSC sẽ hỗ trợ doanh nghiệp một cách toàn diện thông qua hoạt động
+							cung cấp dịch vụ trọn gói, từ tư vấn xây dựng phương án, cấu trúc
+							giao dịch, chuẩn bị hồ sơ, cho đến tư vấn đàm phán điều kiện điều
+							khoản, và các công tác hỗ trợ sau giao dịch.
+						</div>
+						<div class="mt-8">
+							<a href=""
+								class="btn-base-yellow px-6 py-4 inline-flex items-center gap-x-3">
+								<?php echo svg( 'arrow-btn', '16', '16' ) ?>
+								Liên hệ tư vấn
+							</a>
+						</div>
+					</div>
+					<div class="col-span-1 xl:pl-[22px]">
+						<div class="relative overflow-hidden pt-[65.743%] w-full rounded-2xl">
+							<img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/ba1.jpg"
+								alt=""
+								class="absolute w-full h-full inset-0 object-cover transition-all duration-500 hover:scale-105">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="hidden tab-content" id="ma-tab">
+		<div class="xl:my-[100px] md:my-20 my-10">
+			<div class="container">
+				<div class="lg:grid lg:grid-cols-2 xl:gap-0 gap-10">
+					<div class="col-span-1 xl:pr-[42px]">
+						<h2 class="heading-title mb-4">
+							TƯ VẤN MUA BÁN SÁT NHẬP (M&A)
+						</h2>
+						<p class="mb-4 font-bold">BSC cung cấp đa dạng các dịch vụ thị trường
+							vốn
+							bao gồm:</p>
+						<ul class="flex-1 space-y-4 list-icon">
+							<li class="font-semibold list-icon-item">
+								Tư vấn cổ phần hóa
+							</li>
+							<li class="font-semibold list-icon-item">
+								Tư vấn chào bán cổ phần lần đầu ra công chúng (IPO)
+							</li>
+							<li class="font-semibold list-icon-item">
+								Tư vấn phát hành riêng lẻ cổ phiếu
+							</li>
+							<li class="font-semibold list-icon-item">
+								Tư vấn đăng ký niêm yết cổ phiểu
+							</li>
+						</ul>
+						<div class="font-bold mt-4 text-justify">
+							Với nhiều năm kinh nghiệm trên thị trường, đội ngũ chuyên gia của
+							BSC sẽ hỗ trợ doanh nghiệp một cách toàn diện thông qua hoạt động
+							cung cấp dịch vụ trọn gói, từ tư vấn xây dựng phương án, cấu trúc
+							giao dịch, chuẩn bị hồ sơ, cho đến tư vấn đàm phán điều kiện điều
+							khoản, và các công tác hỗ trợ sau giao dịch.
+						</div>
+						<div class="mt-8">
+							<a href=""
+								class="btn-base-yellow px-6 py-4 inline-flex items-center gap-x-3">
+								<?php echo svg( 'arrow-btn', '16', '16' ) ?>
+								Liên hệ tư vấn
+							</a>
+						</div>
+					</div>
+					<div class="col-span-1 xl:pl-[22px]">
+						<div class="relative overflow-hidden pt-[65.743%] w-full rounded-2xl">
+							<img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/ba1.jpg"
+								alt=""
+								class="absolute w-full h-full inset-0 object-cover transition-all duration-500 hover:scale-105">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
 </main>
 <?php
 get_footer();
