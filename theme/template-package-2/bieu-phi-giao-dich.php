9bdc3ae8d88435cc91cb4d3802a1e51b6903de8e
duonglq.dev <duonglq.dev@gmail.com>
2024-11-01 13:59:10 +0700

    done html bieu-phi

---
+++ theme/template-package-2/bieu-phi-giao-dich.php
@@ -0,0 +1,137 @@
+<?php
+
+/**
+Template Name: [Package-2] Biểu phí giao dịch
+ */
+
+get_header();
+?>
+<main>
+	<?php get_template_part( 'components/page-banner' ) ?>
+	<section class="2xl:py-4 py-3 bg-primary-50 sticky z-10 top-0">
+		<div class="container">
+			<ul class="customtab-nav flex justify-between gap-10">
+				<li class="flex-1">
+					<a href="#"
+						class="block text-center font-bold lg:text-lg lg:py-[12px] py-3 px-10 [&:not(.active)]:text-black text-white [&:not(.active)]:bg-transparent bg-primary-300 transition-all duration-500 hover:!text-white hover:!bg-primary-300 rounded-lg">
+						Sổ tay giao dịch
+					</a>
+				</li>
+				<li class="flex-1">
+					<a href="#"
+						class="active block text-center font-bold lg:text-lg lg:py-[12px] py-3 px-10 [&:not(.active)]:text-black text-white [&:not(.active)]:bg-transparent bg-primary-300 transition-all duration-500 hover:!text-white hover:!bg-primary-300 rounded-lg">
+						Biểu phí giao dịch
+					</a>
+				</li>
+				<li class="flex-1">
+					<a href="#"
+						class="block text-center font-bold lg:text-lg lg:py-[12px] py-3 px-10 [&:not(.active)]:text-black text-white [&:not(.active)]:bg-transparent bg-primary-300 transition-all duration-500 hover:!text-white hover:!bg-primary-300 rounded-lg">
+						Điều khoản chung
+					</a>
+				</li>
+
+			</ul>
+		</div>
+	</section>
+	<section class="mt-14 xl:mb-[100px] mb-20">
+		<div class="container">
+			<div class="lg:flex gap-20">
+				<div class="lg:w-[340px] max-w-[35%]">
+					<div class="sticky lg:top-28 top-5 z-[9]">
+						<ul
+							class="shadow-base py-6 pr-4 rounded-lg bg-white space-y-2 sidebar-report">
+							<li>
+								<a href="#"
+									class="flex items-baseline gap-4 2xl:text-lg text-base font-bold [&:not(.active)]:text-black text-white relative py-[12px] px-5 before:w-2 before:shrink-0 before:-translate-y-[3px] before:h-2 before:rounded-[2px] [&:not(.active)]:before:bg-[#051D36] [&:not(.active)]:before:bg-opacity-50 before:bg-white before:bg-opacity-100 bg-primary-300 [&:not(.active)]:bg-white [&:not(.active)]:hover:!bg-[#ebf4fa] rounded-tr-xl rounded-br-xl group-hover:!bg-[#ebf4fa]">Phí
+									giao dịch qua sàn</a>
+
+							</li>
+							<li>
+								<a href="#"
+									class="flex items-baseline gap-4 2xl:text-lg text-base font-bold [&:not(.active)]:text-black text-white relative py-[12px] px-5 before:w-2 before:shrink-0 before:-translate-y-[3px] before:h-2 before:rounded-[2px] [&:not(.active)]:before:bg-[#051D36] [&:not(.active)]:before:bg-opacity-50 before:bg-white before:bg-opacity-100 bg-primary-300 [&:not(.active)]:bg-white [&:not(.active)]:hover:!bg-[#ebf4fa] rounded-tr-xl rounded-br-xl group-hover:!bg-[#ebf4fa]">
+									Phí giao dịch không qua sàn
+								</a>
+							</li>
+							<li>
+								<a href="#"
+									class="flex items-baseline gap-4 2xl:text-lg text-base font-bold [&:not(.active)]:text-black text-white relative py-[12px] px-5 before:w-2 before:shrink-0 before:-translate-y-[3px] before:h-2 before:rounded-[2px] [&:not(.active)]:before:bg-[#051D36] [&:not(.active)]:before:bg-opacity-50 before:bg-white before:bg-opacity-100 bg-primary-300 [&:not(.active)]:bg-white [&:not(.active)]:hover:!bg-[#ebf4fa] rounded-tr-xl rounded-br-xl group-hover:!bg-[#ebf4fa]">Dịch
+									vụ lưu ký</a>
+							</li>
+							<li>
+								<a href="#"
+									class="flex items-baseline gap-4 2xl:text-lg text-base font-bold [&:not(.active)]:text-black text-white relative py-[12px] px-5 before:w-2 before:shrink-0 before:-translate-y-[3px] before:h-2 before:rounded-[2px] [&:not(.active)]:before:bg-[#051D36] [&:not(.active)]:before:bg-opacity-50 before:bg-white before:bg-opacity-100 bg-primary-300 [&:not(.active)]:bg-white [&:not(.active)]:hover:!bg-[#ebf4fa] rounded-tr-xl rounded-br-xl group-hover:!bg-[#ebf4fa]">Dịch
+									vụ chuyển tiền</a>
+							</li>
+							<li>
+								<a href="#"
+									class="flex items-baseline gap-4 2xl:text-lg text-base font-bold [&:not(.active)]:text-black text-white relative py-[12px] px-5 before:w-2 before:shrink-0 before:-translate-y-[3px] before:h-2 before:rounded-[2px] [&:not(.active)]:before:bg-[#051D36] [&:not(.active)]:before:bg-opacity-50 before:bg-white before:bg-opacity-100 bg-primary-300 [&:not(.active)]:bg-white [&:not(.active)]:hover:!bg-[#ebf4fa] rounded-tr-xl rounded-br-xl group-hover:!bg-[#ebf4fa]">Dịch
+									vụ tài khoản</a>
+							</li>
+							<li>
+								<a href="#"
+									class="flex items-baseline gap-4 2xl:text-lg text-base font-bold [&:not(.active)]:text-black text-white relative py-[12px] px-5 before:w-2 before:shrink-0 before:-translate-y-[3px] before:h-2 before:rounded-[2px] [&:not(.active)]:before:bg-[#051D36] [&:not(.active)]:before:bg-opacity-50 before:bg-white before:bg-opacity-100 bg-primary-300 [&:not(.active)]:bg-white [&:not(.active)]:hover:!bg-[#ebf4fa] rounded-tr-xl rounded-br-xl group-hover:!bg-[#ebf4fa]">Dịch
+									vụ tiếng Nhật</a>
+							</li>
+							<li>
+								<a href="#"
+									class="flex items-baseline gap-4 2xl:text-lg text-base font-bold [&:not(.active)]:text-black text-white relative py-[12px] px-5 before:w-2 before:shrink-0 before:-translate-y-[3px] before:h-2 before:rounded-[2px] [&:not(.active)]:before:bg-[#051D36] [&:not(.active)]:before:bg-opacity-50 before:bg-white before:bg-opacity-100 bg-primary-300 [&:not(.active)]:bg-white [&:not(.active)]:hover:!bg-[#ebf4fa] rounded-tr-xl rounded-br-xl group-hover:!bg-[#ebf4fa]">Phí
+									giao dịch hợp đồng tương lai</a>
+							</li>
+							<li class="active">
+								<a href="#"
+									class="active flex items-baseline gap-4 2xl:text-lg text-base font-bold [&:not(.active)]:text-black text-white relative py-[12px] px-5 before:w-2 before:shrink-0 before:-translate-y-[3px] before:h-2 before:rounded-[2px] [&:not(.active)]:before:bg-[#051D36] [&:not(.active)]:before:bg-opacity-50 before:bg-white before:bg-opacity-100 bg-primary-300 [&:not(.active)]:bg-white [&:not(.active)]:hover:!bg-[#ebf4fa] rounded-tr-xl rounded-br-xl group-hover:!bg-[#ebf4fa]">Biểu
+									phí lãi suất</a>
+							</li>
+						</ul>
+
+					</div>
+				</div>
+				<div class="flex-1">
+					<h2 class="heading-title mb-6">
+						BIỂU PHÍ GIAO DỊCH CHỨNG KHOÁN CƠ SỞ
+					</h2>
+					<div
+						class="prose-table:border-collapse prose-table:mb-10 prose-td:border-[4px] prose-td:py-5 prose-td:px-7 prose-td:border-white prose-table:rounded-3xl prose-table:overflow-hidden prose-table:max-w-full prose-table:text-center custom-table prose-ul:pl-5 prose-ul:list-disc prose-ul:mb-6 prose-ul:text-xl prose-table:font-Helvetica">
+						<?php the_content() ?>
+					</div>
+					<div class="mt-10 text-right">
+						<a href="" class="inline-block px-6 py-3 btn-base-yellow" target="_blank">
+							<span class="inline-flex items-center gap-2 relative z-10">
+								<?php _e( 'Tải xuống tài liệu', 'bsc' ) ?>
+								<?php echo svgpath( 'download', '', '', 'fill-black' ) ?>
+							</span>
+						</a>
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<div class="py-20 bg-gradient-blue-250">
+		<div class="container">
+			<div class="lg:flex">
+				<div class="2xl:pr-[50px] pr-10  lg:w-[575px] lg:max-w-[43%] shrink-0">
+					<h2 class="heading-title 2xl:mb-[72px] mb-10">
+						BẠN CẦN HỖ TRỢ?
+					</h2>
+					<div class="relative w-full pt-[66.666%] overflow-hidden rounded-[10px]">
+						<img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/img-contact.png"
+							alt=""
+							class="absolute w-full h-full inset-0 object-cover transition-all duration-500 hover:scale-105">
+					</div>
+				</div>
+				<div class="flex-1 2xl:pl-[50px] pl-10 lg:border-l border-[#D2D2D2]">
+					<h3 class="mb-6 text-primary-300 font-bold xl:text-2xl text-xl">
+						<?php _e( 'Đăng ký thông tin hỗ trợ', 'gnws' ) ?>
+					</h3>
+					<div
+						class="form_support relative font-Helvetica bg-white lg:px-8 px-5 lg:py-6 py-4 rounded-2xl">
+						<?php echo do_shortcode( '[contact-form-7 id="d5b6a0a" title="Đăng ký thông tin hỗ trợ"]' ) ?>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</main>
+<?php
+get_footer();
