06dfa8ff744c8d52b04a69fe190f94c36a81acf3
duonglq.dev <duonglq.dev@gmail.com>
2024-11-07 17:42:09 +0700

    done html single chuong-trinh-khuyen-mai

---
+++ theme/template-package-2/chuong-trinh-khuyen-mai-single.php
@@ -0,0 +1,172 @@
+<?php
+
+/**
+Template Name: [Package-2] Chi tiết chương trình KM
+ */
+
+get_header();
+?>
+<main>
+	<?php get_template_part( 'components/page-banner' ) ?>
+	<section class="mt-12 xl:mb-[100px] mb-12">
+		<div class="container">
+			<div class="lg:flex gap-[70px]">
+				<div class="lg:w-80 max-w-[35%]">
+					<div class="sticky lg:top-28 top-5 z-[9]">
+						<img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/image 105.png"
+							alt="" class="w-full rounded-lg">
+					</div>
+				</div>
+				<div class="flex-1">
+					<h1 class="xl:text-4xl text-3xl mb-8 font-bold">
+						Ưu đãi đặc biệt khi mở tài khoản BSC dành cho các hội viên FireAnt
+					</h1>
+					<div class="lg:flex items-center justify-between mb-8">
+						<div class="">
+							<div class="mt-4 flex items-center gap-2 font-Helvetica">
+								<div class="inline-flex items-center gap-2">
+									<?php echo svg( 'time' ) ?>
+									Thời gian chương trình:
+								</div>
+								<div class="font-medium">22/08/2024 - 22/10/2024</div>
+							</div>
+							<div class="mt-[14px] font-Helvetica xl:max-w-[433px]">
+								<div
+									class="relative bg-[#D9D9D9] rounded-[28px] overflow-hidden h-[5px]">
+									<p class="absolute max-w-full h-full bg-gradient-blue rounded-[28px]"
+										style="width:60%"></p>
+								</div>
+								<div class="mt-2 text-xs">
+									Thời gian khuyến mãi còn <strong class="text-primary-300">20
+										ngày</strong>
+								</div>
+							</div>
+						</div>
+						<div class="share flex items-center gap-[12px] ml-12">
+							<strong>
+								<?php _e( 'Chia sẻ:', 'bsc' ) ?>
+							</strong>
+							<ul class="flex items-center gap-3">
+								<li>
+									<a href="">
+										<?php echo svg( 'ins' ) ?>
+									</a>
+								</li>
+								<li>
+									<a href="">
+										<?php echo svg( 'linkedin' ) ?>
+									</a>
+								</li>
+								<li>
+									<a href="">
+										<?php echo svg( 'fb' ) ?>
+									</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div class="text-[#4A5568] font-Helvetica the_content">
+						<p>
+							Với mục tiêu nâng cao trải nghiệm tài chính cho các nhà đầu tư, từ ngày
+							16/09/2024, CTCP Chứng khoán BIDV (BSC) kết hợp cùng FireAnt, hân hạnh
+							mang đến chương trình khuyến mãi hấp dẫn “Mở tài khoản BSC – Nhận quà
+							liền tay”.
+						</p>
+						<img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/Rectangle 5827.png"
+							alt="">
+						<p>
+							Nội dung về “Giới thiệu về Nghiệp vụ mua bán, sát nhập”
+							Với kinh nghiệm làm việc với các doanh nghiệp trong và ngoài nước, NVS
+							đã trở thành cầu nối uy tín giúp khách hàng tìm được đối tác kinh doanh
+							phù hợp. Cùng với đó, Chúng tôi cung cấp cho khách hàng dịch vụ tư vấn
+							M&A trọn gói, cùng các giải pháp toàn diện và dịch vụ hỗ trợ đồng bộ.
+						</p>
+						<p>Kinh nghiệm của NVS trong lĩnh vực mua bán, sát nhập.
+							Khi tư vấn cho bên mua, NVS không chỉ giới thiệu các cơ hội đầu tư mà
+							còn giúp khách hàng phân tích và có cái nhìn sâu sắc về ngành nghề kinh
+							doanh, các đối thủ cạnh tranh, các rủi ro có thể phát sinh cho thương vụ
+							đầu tư. NVS giúp bên mua xây dựng chiến lược đầu tư và thoái vốn phù hợp
+							với tỷ suất lợi nhuận khách hàng mong muốn đạt được.</p>
+						<p>
+							Khi tư vấn cho bên bán, NVS không chỉ giới thiệu các nhà đầu tư tiềm
+							năng mà còn đồng hành cùng khách hàng trong việc xác định giá trị doanh
+							nghiệp, từ đó đưa ra gói tư vấn toàn diện bao gồm: tư vấn tái cơ cấu tài
+							chính, thẩm định doanh nghiệp, lập kế hoạch phát triển doanh nghiệp, xây
+							dựng chiến lược giá và chiến lược thương lượng, hỗ trợ trong việc đàm
+							phán và lựa chọn nhà đầu tư.
+						</p>
+
+						<p>
+							Nội dung về “Giới thiệu về Nghiệp vụ mua bán, sát nhập”
+							Với kinh nghiệm làm việc với các doanh nghiệp trong và ngoài nước, NVS
+							đã trở thành cầu nối uy tín giúp khách hàng tìm được đối tác kinh doanh
+							phù hợp. Cùng với đó, Chúng tôi cung cấp cho khách hàng dịch vụ tư vấn
+							M&A trọn gói, cùng các giải pháp toàn diện và dịch vụ hỗ trợ đồng bộ.
+						</p>
+						<p>Kinh nghiệm của NVS trong lĩnh vực mua bán, sát nhập.
+							Khi tư vấn cho bên mua, NVS không chỉ giới thiệu các cơ hội đầu tư mà
+							còn giúp khách hàng phân tích và có cái nhìn sâu sắc về ngành nghề kinh
+							doanh, các đối thủ cạnh tranh, các rủi ro có thể phát sinh cho thương vụ
+							đầu tư. NVS giúp bên mua xây dựng chiến lược đầu tư và thoái vốn phù hợp
+							với tỷ suất lợi nhuận khách hàng mong muốn đạt được.</p>
+						<p>
+							Khi tư vấn cho bên bán, NVS không chỉ giới thiệu các nhà đầu tư tiềm
+							năng mà còn đồng hành cùng khách hàng trong việc xác định giá trị doanh
+							nghiệp, từ đó đưa ra gói tư vấn toàn diện bao gồm: tư vấn tái cơ cấu tài
+							chính, thẩm định doanh nghiệp, lập kế hoạch phát triển doanh nghiệp, xây
+							dựng chiến lược giá và chiến lược thương lượng, hỗ trợ trong việc đàm
+							phán và lựa chọn nhà đầu tư.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="xl:my-[100px] my-20">
+		<div class="container">
+			<h2 class="heading-title mb-10">KHUYẾN MÃI LIÊN QUAN</h2>
+
+			<div class="grid lg:grid-cols-3 grid-cols-1 gap-[21px]">
+				<?php
+				for ( $i = 0; $i < 3; $i++ )
+				{
+					?>
+					<div class="flex flex-col font-Helvetica">
+						<a href="" class="block w-full pt-[64.66%] overflow-hidden rounded-xl relative">
+							<img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/image2.png"
+								alt=""
+								class="absolute w-full h-full inset-0 object-cover hover:scale-105 transition-all duration-500">
+						</a>
+						<h3
+							class="mt-8 font-bold lg:text-lg transition-all duration-500 hover:text-green">
+							<a href="" class="line-clamp-2">
+								“Giao dịch ngay - Quay là trúng” cùng BSC WebTrading
+							</a>
+						</h3>
+						<div class="mt-6 flex items-center gap-4">
+							<div class="inline-flex items-center gap-2">
+								<?php echo svg( 'time' ) ?>
+								Thời gian chương trình:
+							</div>
+							<div class="font-medium">22/08/2024 - 22/10/2024</div>
+						</div>
+						<div class="mt-[14px]">
+							<div class="relative bg-[#D9D9D9] rounded-[28px] overflow-hidden h-[5px]">
+								<p class="absolute max-w-full h-full bg-gradient-blue rounded-[28px]"
+									style="width:60%"></p>
+							</div>
+							<div class="mt-2 text-xs">
+								Thời gian khuyến mãi còn <strong class="text-primary-300">20
+									ngày</strong>
+							</div>
+						</div>
+					</div>
+					<?php
+				}
+				?>
+			</div>
+		</div>
+	</section>
+</main>
+<?php
+get_footer();
