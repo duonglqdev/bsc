7cc23d0af7a11270bb211b0f5641fdec4b0a7e0a
duonglq.dev <duonglq.dev@gmail.com>
2024-11-13 09:14:32 +0700

    done chi-tiet-vi-mo

--- theme/template-package-3/chi-tiet-bao-cao.php
+++ theme/template-package-3/chi-tiet-bao-cao.php
@@ -73,17 +73,194 @@ get_header();
 								#Muaban
 							</a>
 						</div>
-                        <div class="mt-6">
-                        <a href=""
-							class="bg-yellow-100 text-black hover:shadow-[0px_4px_16px_0px_rgba(255,184,28,0.5)] hover:bg-[#ffc547] inline-block px-6 py-3 font-semibold relative transition-all duration-500 leading-tight flex-1 rounded-xl w-full h-10 text-center text-xs">
-							<?php _e( 'Tải xuống', 'bsc' ) ?>
-						</a>
-                        </div>
+						<div class="mt-6">
+							<a href=""
+								class="bg-yellow-100 text-black hover:shadow-[0px_4px_16px_0px_rgba(255,184,28,0.5)] hover:bg-[#ffc547] inline-block px-6 py-3 font-semibold relative transition-all duration-500 leading-tight flex-1 rounded-xl w-full h-10 text-center text-xs">
+								<?php _e( 'Tải xuống', 'bsc' ) ?>
+							</a>
+						</div>
 					</div>
 				</div>
-				<div class="flex-1">
-
+				<div class="flex-1 space-y-8">
+					<div class="summary">
+						<h2 class="text-2xl font-bold mb-4">
+							Tóm tắt báo cáo
+						</h2>
+						<div
+							class="text-justify font-Helvetica prose-img:rounded-[10px] prose-img:mx-auto prose-img:mt-6 prose-img:mb-8 prose-ul:pl-3 prose-ul:list-disc prose-ul:space-y-2">
+							Bản báo cáo này của Công ty Cổ phần Chứng khoán BIDV (BSC), chỉ cung cấp
+							những thông tin chung và phân tích về tình hình kinh tế vĩ mô cũng như
+							diễn biến thị trường chứng khoán. Báo cáo này không được xây dựng để
+							cung cấp theo yêu cầu của bất kỳ tổ chức hay cá nhân riêng lẻ nào hoặc
+							các quyết định mua bán, nắm giữ chứng khoán. Nhà đầu tư chỉ nên sử dụng
+							các thông tin, phân tích, bình luận của Bản báo cáo như là nguồn tham
+							khảo trước khi đưa ra những quyết định đầu tư cho riêng mình. Mọi thông
+							tin, nhận định và dự báo và quan điểm trong báo cáo này được dựa trên
+							những nguồn dữ liệu đáng tin cậy. Tuy nhiên Công ty Cổ phần Chứng khoán
+							BIDV (BSC) không đảm bảo rằng các nguồn thông tin này là hoàn toàn chính
+							xác và không chịu bất kỳ một trách nhiệm nào đối với tính chính xác của
+							những thông tin được đề cập đến trong báo cáo này, cũng như không chịu
+							trách nhiệm về những thiệt hại đối với việc sử dụng toàn bộ hay một phần
+							nội dung của bản báo cáo này. Mọi quan điểm cũng như nhận định được đề
+							cập trong báo cáo này dựa trên sự cân nhắc cẩn trọng, công minh và hợp
+							lý nhất trong hiện tại. Tuy nhiên những quan điểm, nhận định này có thể
+							thay đổi mà không cần báo trước. Bản báo cáo này có bản quyền và là tài
+							sản của Công ty Cổ phần Chứng khoán BIDV (BSC). Mọi hành vi sao chép,
+							sửa đổi, in ấn mà không có sự đồng ý của BSC đều trái luật. Bất kỳ nội
+							dung nào của tài liệu này cũng không được (i) sao chụp hay nhân bản ở
+							bất kỳ hình thức hay phương thức nào hoặc (ii) được cung cấp nếu không
+							được sự chấp thuận của CTCP Chứng khoán BIDV
+						</div>
+					</div>
+					<div class="detail">
+						<h2 class="text-2xl font-bold mb-4">
+							Tóm tắt báo cáo
+						</h2>
+						<div
+							class="text-justify font-Helvetica prose-img:rounded-[10px] prose-img:mx-auto prose-img:mt-6 prose-img:mb-8 prose-ul:pl-3 prose-ul:list-disc prose-ul:space-y-2 text-[#000]">
+							<img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/image 109 (1).png"
+								alt="">
+							<ul>
+								<li>SMB: CTCP Bia Sài Gòn – Miền Trung (SMB – sàn HOSE) cho biết,
+									ngày 18/9 tới đây sẽ chốt danh sách cổ đông tạm ứng cổ tức đợt
+									2/2024 bằng tiền mặt với tỷ lệ 20%, tức cổ đông sở hữu 1 cổ
+									phiếu sẽ được nhận 2.000 đồng. </li>
+								<li>DXG: CTCP Tập đoàn Đất Xanh (DXG - sàn HOSE) theo báo cáo tài
+									chính hợp nhất quý II/2024, Đất Xanh ghi nhận doanh thu đạt gần
+									2.191 tỷ đồng, gấp đôi so với cùng kỳ; lợi nhuận sau thuế đạt
+									171,3 tỷ đồng, gấp tới gần 4,3 lần so với cùng kỳ năm ngoái.
+								</li>
+								<li>NKG: CTCP Thép Nam Kim (mã NKG - sàn HOSE) ghi nhận lãi 219,63
+									tỷ đồng trong quý II/2024, luỹ kế nửa đầu năm 2024 lãi 369,7 tỷ
+									đồng, tăng 385,7% so với cùng kỳ. Lý giải lợi nhuận tăng mạnh
+									trong quý II, ông Võ Hoàng Vũ, Tổng giám đốc Thép Nam Kim cho
+									biết, tốc độ tăng của doanh thu cao hơn tốc độ tăng của giá vốn
+									nhờ vào việc Công ty đẩy mạnh các kênh bán hàng trong nước và
+									xuất khẩu. </li>
+								<li>NVL: Báo cáo kết quả kinh doanh quý II/2024 của Novaland ghi
+									nhận khoản chuyển nhượng vốn góp tại CTCP Huỳnh Gia Huy với giá
+									chuyển nhượng chỉ 1,9 tỷ đồng. Giá chuyển nhượng này không bao
+									gồm giá trị các tài sản tiện ích và các nghĩa vụ nợ tồn đọng.
+								</li>
+								<li>VHM: CTCP Vinhomes (VHM) công bố sẽ mua 370 triệu cổ phiếu quỹ,
+									tương đương 8,5% khối lượng cổ phiếu đang lưu hành. Phương thức
+									giao dịch khớp lệnh hoặc thỏa thuận. </li>
+								<li>KDH: Khang Điền (KDH): Nhóm quỹ Dragon hoàn tất mua vào tổng
+									cộng hơn 20 triệu cổ phiếu. Cụ thể, quỹ thành viên Amersham
+									Industries Limited mua 5,1 triệu cổ phiếu, Vietnam Enterprise
+									Investments Limited mua 5 triệu cổ phiếu, Hanoi Investments
+									Holdings Limited mua 4,41 triệu cổ phiếu, Norges Bank mua vào
+									3,1 triệu cổ phiếu...</li>
+							</ul>
+						</div>
+					</div>
 				</div>
+			</div>
+		</div>
+	</section>
+	<section class="xl:my-[100px] my-20">
+		<div class="container">
+			<h3 class="text-center font-bold lg:text-[32px] text-2xl mb-8">
+				Các báo cáo liên quan
+			</h3>
+			<div class="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6">
+				<?php
+				for ( $i = 0; $i < 2; $i++ )
+				{
+					?>
+					<div class="rounded-[10px] bg-white shadow-base-sm px-6 py-4 flex flex-col">
+						<div class="flex items-center justify-between mb-4">
+							<a href=""
+								class="inline-block bg-primary-300 text-white px-3 py-1 rounded transition-all duration-500 hover:bg-primary-600 text-xs font-semibold">
+								Báo cáo vĩ mô tuần
+							</a>
+							<div class="flex flex-col gap-1.5">
+								<span
+									class="inline-block rounded-[45px] text-[#30D158] bg-[#D6F6DE] px-4 py-0.5 text-[12px] font-semibold">Tích
+									cực</span>
+								<p class="text-paragraph text-xs font-Helvetica">22/10/2024</p>
+							</div>
+						</div>
+						<h3 class="font-bold mb-6 transition-all duration-500 hover:text-green">
+							<a href="" class="line-clamp-2">
+								Daily Morning_VHM công bố sẽ mua 370 triệu Cổ phiếu
+								quỹ_20240808
+							</a>
+						</h3>
+						<div class="flex items-center justify-between">
+							<p class="italic text-paragraph text-xs font-Helvetica">
+								68 Lượt tải xuống
+							</p>
+							<a href=""
+								class="inline-flex items-center gap-3 text-green font-bold transition-all duration-500 hover:scale-105">
+								<?php _e( 'Tải xuống', 'bsc' ) ?>
+								<?php echo svg( 'download', '20', '20' ) ?>
+							</a>
+						</div>
+					</div>
+					<div class="rounded-[10px] bg-white shadow-base-sm px-6 py-4 flex flex-col">
+						<div class="flex items-center justify-between mb-4">
+							<a href=""
+								class="inline-block bg-primary-300 text-white px-3 py-1 rounded transition-all duration-500 hover:bg-primary-600 text-xs font-semibold">
+								Báo cáo ngành
+							</a>
+							<div class="flex flex-col gap-1.5">
+								<span
+									class="inline-block rounded-[45px] text-[#FF0017] bg-[#FFD9DC] px-4 py-0.5 text-[12px] font-semibold">Tiêu
+									cực</span>
+								<p class="text-paragraph text-xs font-Helvetica">22/10/2024</p>
+							</div>
+						</div>
+						<h3 class="font-bold mb-6 transition-all duration-500 hover:text-green">
+							<a href="" class="line-clamp-2">
+								Daily Morning_VHM công bố sẽ mua 370 triệu Cổ phiếu
+								quỹ_20240808
+							</a>
+						</h3>
+						<div class="flex items-center justify-between">
+							<p class="italic text-paragraph text-xs font-Helvetica">
+								68 Lượt tải xuống
+							</p>
+							<a href=""
+								class="inline-flex items-center gap-3 text-green font-bold transition-all duration-500 hover:scale-105">
+								<?php _e( 'Tải xuống', 'bsc' ) ?>
+								<?php echo svg( 'download', '20', '20' ) ?>
+							</a>
+						</div>
+					</div>
+					<div class="rounded-[10px] bg-white shadow-base-sm px-6 py-4 flex flex-col">
+						<div class="flex items-center justify-between mb-4">
+							<a href=""
+								class="inline-block bg-primary-300 text-white px-3 py-1 rounded transition-all duration-500 hover:bg-primary-600 text-xs font-semibold">
+								Báo cáo ngành
+							</a>
+							<div class="flex flex-col gap-1.5">
+								<span
+									class="inline-block rounded-[45px] text-[#FFB81C] bg-[#FFF1D2] px-4 py-0.5 text-[12px] font-semibold">Trung
+									lập</span>
+								<p class="text-paragraph text-xs font-Helvetica">22/10/2024</p>
+							</div>
+						</div>
+						<h3 class="font-bold mb-6 transition-all duration-500 hover:text-green">
+							<a href="" class="line-clamp-2">
+								Daily Morning_VHM công bố sẽ mua 370 triệu Cổ phiếu
+								quỹ_20240808
+							</a>
+						</h3>
+						<div class="flex items-center justify-between mt-auto">
+							<p class="italic text-paragraph text-xs font-Helvetica">
+								68 Lượt tải xuống
+							</p>
+							<a href=""
+								class="inline-flex items-center gap-3 text-green font-bold transition-all duration-500 hover:scale-105">
+								<?php _e( 'Tải xuống', 'bsc' ) ?>
+								<?php echo svg( 'download', '20', '20' ) ?>
+							</a>
+						</div>
+					</div>
+					<?php
+				}
+				?>
 			</div>
 		</div>
 	</section>
