93b8b3497210f22bb4f94ac0f7bf793983bf789e
Gini Webseo <87066567+giniwebseo@users.noreply.github.com>
2024-11-26 15:53:06 +0700

    Update chuyên gia

--- theme/inc/customizer-cf7.php
+++ theme/inc/customizer-cf7.php
@@ -30,3 +30,21 @@ function create_shortcode_email_admin()
     return $list_post;
 }
 add_shortcode('email_admin', 'create_shortcode_email_admin');
+
+/**
+ * Translate text CF7
+ */
+add_filter('wpcf7_form_elements', function ($form) {
+    // Tìm placeholder trong định dạng {Your Text}
+    $pattern = '/\{([^\}]+)\}/';
+    $form = preg_replace_callback($pattern, function ($matches) {
+        $text = $matches[1]; // Lấy nội dung trong { }
+        // Đăng ký chuỗi dịch động
+        do_action('wpml_register_single_string', 'Contact Form 7', $text, $text);
+
+        // Dịch chuỗi
+        return esc_attr(function_exists('pll__') ? pll__($text) : __($text, 'bsc'));
+    }, $form);
+
+    return $form;
+});
