41326d17423e6ee5cffba3076c30969937eba65a
duonglqdev <duonglq.dev@gmail.com>
2024-12-09 19:03:51 +0700

    fix content editor

--- theme/page.php
+++ theme/page.php
@@ -17,7 +17,7 @@ get_header();
 <main>
 	<div class="container">
 		<div class="my-20">
-			<div class="font-Helvetica content_prose prose-ul:pl-7 prose-ul:list-disc prose-ol:pl-6 prose-ol:list-decimal prose-ul:mb-3 prose-ul:mt-2 prose-ol:mb-2 prose-table:my-5 prose-table:border-collapse prose-table:border prose-table:border-slate-300  prose-table:th:border prose-th:border-slate-300 prose-td:p-3 prose-td:border prose-td:border-slate-300 prose-p:mb-2">
+			<div class="font-Helvetica content_prose prose-ul:pl-7 prose-ol:pl-7 prose-ul:mb-3 prose-ul:mt-2 prose-ol:mb-2 prose-table:my-5 prose-table:border-collapse prose-table:border prose-table:border-slate-300  prose-table:th:border prose-th:border-slate-300 prose-td:p-3 prose-td:border prose-td:border-slate-300 prose-p:mb-2">
 				<?php the_content() ?>
 			</div>
 		</div>
