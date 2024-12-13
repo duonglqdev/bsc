7c01e4ebf3a034f13d7fd7020bd3ce7fa1205e2d
duonglq.dev <duonglq.dev@gmail.com>
2024-12-13 17:25:15 +0700

    done mobile biểu phí giao dịch

--- theme/components-bpgd/table_1.php
+++ theme/components-bpgd/table_1.php
@@ -1,57 +1,59 @@
-<div class="table__item mb-10 table_1">
+<div class="table__item table_1 <?php echo !wp_is_mobile() && !bsc_is_mobile() ?'mb-10':'mb-6' ?>">
     <?php if (get_sub_field('title')) { ?>
-        <ul class="pl-5 list-disc mb-6 font-bold lg:text-xl">
+        <ul class="pl-5 list-disc font-bold <?php echo !wp_is_mobile() && !bsc_is_mobile() ?'text-xl mb-6':'mb-4' ?>">
             <li>
                 <?php the_sub_field('title') ?>
             </li>
         </ul>
     <?php } ?>
-    <table>
-        <?php if (have_rows('table_title')) {
-            while (have_rows('table_title')): the_row(); ?>
-                <thead>
-                    <tr>
-                        <th rowspan="2" class="w-[7.313%] px-0"><?php _e('STT', 'bsc') ?></th>
-                        <th rowspan="2" class="w-[36.72%]"><?php the_sub_field('cot_1') ?></th>
-                        <th colspan="2" class="w-[55.93%] !py-2"><?php the_sub_field('cot_2_title') ?></th>
-                    </tr>
-                    <tr>
-                        <?php
-                        if (have_rows('cot_2')) {
-                            while (have_rows('cot_2')): the_row();
-                        ?>
-                                <th class="w-1/2 !py-2"><?php the_sub_field('cot_2_1') ?></th>
-                                <th class="w-1/2 !py-2"><?php the_sub_field('cot_2_2') ?></th>
-                        <?php
-                            endwhile;
-                        }
-                        ?>
-                    </tr>
-                </thead>
-        <?php
-            endwhile;
-        }
-        ?>
-        <?php if (have_rows('table_re')) { ?>
-            <tbody>
-                <?php
-                $i = 0;
-                while (have_rows('table_re')): the_row();
-                    $i++;
-                    $color = ($i % 2 == 1) ? '#EBF4FA' : '#DFF2FF';
-                ?>
-                    <tr>
-                        <td class="centered" style="background-color:<?php echo $color ?>;"><?php echo $i ?></td>
-                        <td style="background-color:<?php echo $color ?>;"><?php the_sub_field('cot_1') ?></td>
-                        <td style="background-color:<?php echo $color ?>;"><?php the_sub_field('cot_2') ?></td>
-                        <td style="background-color:<?php echo $color ?>;"><?php the_sub_field('cot_3') ?></td>
-                    </tr>
-                <?php endwhile; ?>
-            </tbody>
-        <?php } ?>
-    </table>
+    <div class="<?php echo !wp_is_mobile() && !bsc_is_mobile() ?'':'overflow-x-auto' ?>">
+        <table>
+            <?php if (have_rows('table_title')) {
+                while (have_rows('table_title')): the_row(); ?>
+                    <thead>
+                        <tr>
+                            <th rowspan="2" class="<?php echo !wp_is_mobile() && !bsc_is_mobile() ?'w-[7.313%] px-0':'px-2 whitespace-nowrap w-[50px]' ?>"><?php _e('STT', 'bsc') ?></th>
+                            <th rowspan="2" class="<?php echo !wp_is_mobile() && !bsc_is_mobile() ?'w-[36.72%]':'w-[158px]' ?>"><?php the_sub_field('cot_1') ?></th>
+                            <th colspan="2" class="!py-2 <?php echo !wp_is_mobile() && !bsc_is_mobile() ?'w-[55.93%]':'w-[320px]' ?>"><?php the_sub_field('cot_2_title') ?></th>
+                        </tr>
+                        <tr>
+                            <?php
+                            if (have_rows('cot_2')) {
+                                while (have_rows('cot_2')): the_row();
+                            ?>
+                                    <th class="w-1/2 !py-2"><?php the_sub_field('cot_2_1') ?></th>
+                                    <th class="w-1/2 !py-2"><?php the_sub_field('cot_2_2') ?></th>
+                            <?php
+                                endwhile;
+                            }
+                            ?>
+                        </tr>
+                    </thead>
+            <?php
+                endwhile;
+            }
+            ?>
+            <?php if (have_rows('table_re')) { ?>
+                <tbody>
+                    <?php
+                    $i = 0;
+                    while (have_rows('table_re')): the_row();
+                        $i++;
+                        $color = ($i % 2 == 1) ? '#EBF4FA' : '#DFF2FF';
+                    ?>
+                        <tr>
+                            <td class="centered" style="background-color:<?php echo $color ?>;"><?php echo $i ?></td>
+                            <td style="background-color:<?php echo $color ?>;"><?php the_sub_field('cot_1') ?></td>
+                            <td style="background-color:<?php echo $color ?>;"><?php the_sub_field('cot_2') ?></td>
+                            <td style="background-color:<?php echo $color ?>;"><?php the_sub_field('cot_3') ?></td>
+                        </tr>
+                    <?php endwhile; ?>
+                </tbody>
+            <?php } ?>
+        </table>
+    </div>
     <?php if (get_sub_field('content')) { ?>
-        <div class="mt-4 font-Helvetica prose-ul:pl-5 prose-ul:list-disc prose-ul:!text-base">
+        <div class="mt-4 font-Helvetica prose-ul:pl-5 prose-ul:list-disc <?php echo !wp_is_mobile() && !bsc_is_mobile() ?'prose-ul:!text-base':'' ?>">
             <?php the_sub_field('content') ?>
         </div>
     <?php } ?>
--- theme/single-bieu-phi-giao-dich.php
+++ theme/single-bieu-phi-giao-dich.php
@@ -8,33 +8,56 @@ $id_post = get_the_ID();
         'data' => 'bpgd',
     )) ?>
     <h1 class="hidden"><?php the_title() ?></h1>
-    <section class="mt-14 xl:mb-[100px] mb-20">
+    <section class="<?php echo !wp_is_mobile() && !bsc_is_mobile() ?'mt-14 xl:mb-[100px] mb-20':'my-[50px]' ?>">
         <div class="container">
-            <div class="lg:flex gap-20">
-                <div class="lg:w-[340px] max-w-[35%]">
+            <div class=" <?php echo !wp_is_mobile() && !bsc_is_mobile() ?'flex gap-20':'' ?>">
+                <div class="<?php echo !wp_is_mobile() && !bsc_is_mobile() ?'w-[340px] max-w-[35%] shrink-0':'relative' ?>">
                     <?php $query = new WP_Query(array(
                         'post_type' => 'bieu-phi-giao-dich',
                         'post_status' => 'publish',
                         'posts_per_page' => 100,
                     ));
                     if ($query->have_posts()) { ?>
-                        <div class="sticky lg:top-28 top-5 z-[9]">
-                            <ul
-                                class="shadow-base py-6 pr-4 rounded-lg bg-white space-y-2 sidebar-report">
-                                <?php while ($query->have_posts()) : $query->the_post(); ?>
-                                    <li>
-                                        <a href="<?php the_permalink() ?>"
-                                            class="<?php if (get_the_ID() == $id_post) echo 'active' ?> flex items-baseline gap-4 2xl:text-lg text-base font-bold [&:not(.active)]:text-black text-white relative py-[12px] px-5 before:w-2 before:shrink-0 before:-translate-y-[3px] before:h-2 before:rounded-[2px] [&:not(.active)]:before:bg-[#051D36] [&:not(.active)]:before:bg-opacity-50 before:bg-white before:bg-opacity-100 bg-primary-300 [&:not(.active)]:bg-white [&:not(.active)]:hover:!bg-[#ebf4fa] rounded-tr-xl rounded-br-xl group-hover:!bg-[#ebf4fa]">
-                                            <?php the_title() ?>
-                                        </a>
-                                    </li>
-                                <?php endwhile; ?>
-                            </ul>
+                        <div class="sticky z-[9] <?php echo ! wp_is_mobile() && ! bsc_is_mobile() ? 'lg:top-28' : 'top-5' ?>">
+                            <?php if(! wp_is_mobile() && ! bsc_is_mobile()) : ?>
+                                <ul
+                                    class="shadow-base py-6 pr-4 rounded-lg bg-white space-y-2 sidebar-report">
+                                    <?php while ($query->have_posts()) : $query->the_post(); ?>
+                                        <li>
+                                            <a href="<?php the_permalink() ?>"
+                                                class="<?php if (get_the_ID() == $id_post) echo 'active' ?> flex items-baseline gap-4 2xl:text-lg text-base font-bold [&:not(.active)]:text-black text-white relative py-[12px] px-5 before:w-2 before:shrink-0 before:-translate-y-[3px] before:h-2 before:rounded-[2px] [&:not(.active)]:before:bg-[#051D36] [&:not(.active)]:before:bg-opacity-50 before:bg-white before:bg-opacity-100 bg-primary-300 [&:not(.active)]:bg-white [&:not(.active)]:hover:!bg-[#ebf4fa] rounded-tr-xl rounded-br-xl group-hover:!bg-[#ebf4fa]">
+                                                <?php the_title() ?>
+                                            </a>
+                                        </li>
+                                    <?php endwhile; ?>
+                                </ul>
+                            <?php else : ?>
+                                <div
+                                    class="p-[12px] text-xs font-bold text-white bg-primary-300 rounded-lg flex items-center justify-between toggle-next">
+                                    <?php echo get_the_archive_title() ?>
+                                    <?php echo svg( 'down-white', '20' ) ?>
+                                </div>
+                                <ul
+                                    class="overflow-y-auto absolute py-2 z-30 w-full max-h-64 scroll-bar-custom block [&:not(.active)]:opacity-0 opacity-100 [&:not(.active)]:pointer-events-none transition-all duration-500 origin-top-left scale-x-100 [&:not(.active)]:scale-y-0 scale-100 bg-[#F3FBFE] p-2 prose-a:block rounded text-xs">
+                                    <?php while ($query->have_posts()) : $query->the_post(); ?>
+                                        <li>
+                                            <a href="<?php the_permalink() ?>"
+                                                class="<?php if (get_the_ID() == $id_post) echo 'active' ?> text-xs px-3 py-2 rounded-md font-medium [&:not(.active)]:text-black text-white [&:not(.active)]:bg-white bg-primary-300">
+                                                <?php the_title() ?>
+                                            </a>
+                                        </li>
+                                    <?php endwhile; ?>
+                                </ul>
+                            <?php endif; ?>
                         </div>
                     <?php };
                     wp_reset_postdata() ?>
                 </div>
-                <div class="flex-1">
+
+
+
+
+                <div class="flex-1 <?php echo !wp_is_mobile() && !bsc_is_mobile() ?'':'mt-6' ?>">
                     <?php if (get_field('title')) {
                         $title = get_field('title');
                     } else {
@@ -44,7 +67,7 @@ $id_post = get_the_ID();
                         <?php echo $title; ?>
                     </h1>
                     <div
-                        class="prose-table:border-collapse prose-td:border-[4px] prose-th:border-[4px] prose-th:py-5 prose-th:px-5 prose-td:py-5 prose-td:px-[29px] prose-td:border-white prose-th:border-white prose-table:rounded-3xl prose-table:overflow-hidden prose-table:max-w-full prose-table:w-full prose-table:text-center custom-table prose-ul:pl-5 prose-ul:list-disc prose-ul:mb-6 prose-ul:text-xl prose-table:font-Helvetica prose-table:font-medium prose-thead:font-bold prose-table:table-fixed">
+                        class="prose-table:border-collapse prose-td:border-[4px] prose-th:border-[4px] prose-td:border-white prose-th:border-white prose-table:rounded-3xl prose-table:overflow-hidden prose-table:max-w-full prose-table:w-full prose-table:text-center custom-table prose-ul:pl-5 prose-ul:list-disc prose-ul:mb-6  prose-table:font-Helvetica prose-table:font-medium prose-thead:font-bold prose-table:table-fixed <?php echo !wp_is_mobile() && !bsc_is_mobile() ?'prose-ul:text-xl prose-th:py-5 prose-th:px-5 prose-td:py-5 prose-td:px-[29px]':'prose-ul:text-lg prose-th:py-4 prose-th:px-3 prose-td:py-[12px] prose-td:px-3 text-xxs'?>">
                         <?php
                         $page_id = get_the_ID();
                         if (have_rows('home_components_stgd', $page_id)) {
