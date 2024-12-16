9a0df3cb67cf241cba8a30624e1695b6767e6111
Gini Webseo <87066567+giniwebseo@users.noreply.github.com>
2024-12-16 16:53:45 +0700

    https://wework.base.vn/mytasks?task=5497056

--- theme/components/qdbsc_vi_mo.php
+++ theme/components/qdbsc_vi_mo.php
@@ -46,7 +46,7 @@ $class = $check_logout['class'];
                                         </div>
                                         <div
                                             class="min-w-[84px] text-center py-0.5 px-4 text-[#30D158] bg-[#D6F6DE] rounded-[45px] font-semibold text-xs">
-                                            <?php _e('Tích cực', 'bsc') ?>
+                                            <?php _e('Khả quan', 'bsc') ?>
                                         </div>
                                     </div>
 
@@ -71,7 +71,7 @@ $class = $check_logout['class'];
                                         </div>
                                         <div
                                             class="min-w-[84px] text-center py-0.5 px-4 text-[#FF0017] bg-[#FFD9DC] rounded-[45px] font-semibold text-xs">
-                                            <?php _e('Tiêu cực', 'bsc') ?>
+                                            <?php _e('Kém khả quan', 'bsc') ?>
                                         </div>
                                     </div>
 
@@ -105,7 +105,7 @@ $class = $check_logout['class'];
                                             ---- </h4>
                                     </div>
                                     <div class="min-w-[84px] text-center py-0.5 px-4 text-[#30D158] bg-[#D6F6DE] rounded-[45px] font-semibold text-xs">
-                                        <?php _e('Tích cực', 'bsc') ?> </div>
+                                        <?php _e('Khả quan', 'bsc') ?> </div>
                                 </div>
 
                                 <div class="flex items-end justify-between pb-2">
@@ -124,7 +124,7 @@ $class = $check_logout['class'];
                                             ---- </h4>
                                     </div>
                                     <div class="min-w-[84px] text-center py-0.5 px-4 text-[#FF0017] bg-[#FFD9DC] rounded-[45px] font-semibold text-xs">
-                                        <?php _e('Tiêu cực', 'bsc') ?> </div>
+                                        <?php _e('Kém khả quan', 'bsc') ?> </div>
                                 </div>
                             </div>
                         </div>
