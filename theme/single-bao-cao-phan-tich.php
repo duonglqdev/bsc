923cbadcca1641644fdaa08e1717aa2c247c0900
Gini Webseo <87066567+giniwebseo@users.noreply.github.com>
2024-12-17 16:29:51 +0700

    Update tải báo cáo phân tích

--- theme/single-bao-cao-phan-tich.php
+++ theme/single-bao-cao-phan-tich.php
@@ -160,7 +160,7 @@ get_header();
                             $viewerpermission = $news->viewerpermission;
                             if ($viewerpermission == 'USER_BSC') {
                                 $datetimeopen = $news->datetimeopen;
-                                if (is_null($datetimeopen) || strtotime($datetimeopen) < time()) {
+                                if (is_null($datetimeopen) || strtotime($datetimeopen) > time()) {
                                     if (bsc_is_user_logged_out()) {
                                         $count_download = false;
                                         $url_download = bsc_url_sso();
