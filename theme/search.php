c55a071663d5af4a7ed401939df6a5df4e9b754c
duonglq.dev <duonglq.dev@gmail.com>
2024-12-24 11:48:00 +0700

    done mobile search

--- theme/search.php
+++ theme/search.php
@@ -17,19 +17,23 @@ $type_search =  $_GET['type_search'] ?: 'default';
 	<section class="py-[88px] bg-no-repeat bg-cover"
 		style="background-image:url(<?php echo get_stylesheet_directory_uri() ?>/assets/svg/bg-search.svg)">
 		<div class="container">
-			<div class="text-center">
-				<?php if (function_exists('rank_math_the_breadcrumbs'))
-					rank_math_the_breadcrumbs(); ?>
-			</div>
-			<h2 class="heading-title text-center mt-6 mb-8">
+			<?php if ( !wp_is_mobile() && !bsc_is_mobile()) { ?> 
+				<div class="text-center">
+					<?php if (function_exists('rank_math_the_breadcrumbs'))
+						rank_math_the_breadcrumbs(); ?>
+				</div>
+			<?php } ?>
+			<h2 class="heading-title text-center <?php echo !wp_is_mobile() && !bsc_is_mobile() ?'mt-6 mb-8':'mb-6' ?>">
 				<?php _e('BẠN MUỐN TÌM KIẾM ĐIỀU GÌ?', 'bsc') ?>
 			</h2>
 			<form action="<?php echo get_home_url() ?>"
 				class="flex gap-3 justify-center form-search-result">
-				<div class="w-[660px] max-w-[90%] py-4 px-6 gap-2 flex h-[58px] bg-white rounded-lg overflow-hidden shadow-base">
-					<?php echo svgpath('search', '24', '24', 'fill-[#4a556880]') ?>
+				<div class="flex items-center bg-white rounded-lg overflow-hidden shadow-base <?php echo !wp_is_mobile() && !bsc_is_mobile() ?'h-[58px] w-[660px] max-w-[90%] gap-2 py-4 px-6 ':'h-12 flex-1 gap-4 py-3 px-4' ?>">
+					<p class="<?php echo !wp_is_mobile() && !bsc_is_mobile() ?'w-6 h-6':'w-5 h-5' ?>">
+						<?php echo svgpath('search', '', '', 'fill-[#4a556880]') ?>
+					</p>
 					<input type="text" name="s"
-						class="flex-1 border-none focus:border-none focus:outline-0 focus:ring-0 font-Helvetica placeholder:text-paragraph placeholder:text-opacity-50 form-search-input"
+						class="flex-1 border-none focus:border-none focus:outline-0 focus:ring-0 font-Helvetica placeholder:text-paragraph placeholder:text-opacity-50 form-search-input p-0"
 						placeholder="<?php _e('Bạn muốn tìm kiếm...', 'bsc') ?>" value="<?php echo $search ?>">
 					<?php if ($type_search && $type_search != 'default') {
 					?>
@@ -39,91 +43,94 @@ $type_search =  $_GET['type_search'] ?: 'default';
 
 				</div>
 				<a type="reset" href="<?php echo get_home_url() . '?s=' ?>"
-					class="w-[58px] h-[58px] rounded-lg flex items-center justify-center p-3 bg-white group">
+					class="<?php echo !wp_is_mobile() && !bsc_is_mobile() ?'w-[58px] h-[58px]':'w-12 h-12' ?> shrink-0 rounded-lg flex items-center justify-center p-3 bg-white group">
 					<?php echo svgClass('reload', '24', '24', 'transition-all duration-500 group-hover:rotate-[360deg] will-change-transform') ?>
 				</a>
 			</form>
 		</div>
 	</section>
-	<section class="xl:[my-100px] my-20">
+	<section class="<?php echo !wp_is_mobile() && !bsc_is_mobile() ?'xl:[my-100px] my-20':'my-[50px]' ?>">
 		<div class="container">
 			<div class="lg:flex gap-[70px] max-w-[1112px] mx-auto">
-				<div class="lg:w-[290px]">
-					<ul
-						class="flex flex-col py-[15px] pr-[15px] rounded-[15px] space-y-3 shadow-base sticky top-5 z-10">
-						<li>
-							<a href="<?php echo $current_url ?>?s=<?php echo $search ?>"
-								class="<?php if ($type_search == 'default') echo 'active' ?> flex items-center justify-between w-full px-5 py-[15px] lg:text-lg font-semibold transition-all text-left hover:!text-white hover:!bg-primary-400 hover:!rounded-tr-xl hover:!rounded-br-xl [&:not(.active)]:text-black text-white [&:not(.active)]:bg-white bg-primary-400 [&:not(.active)]:rounded-br-none rounded-br-xl [&:not(.active)]:rounded-tr-none rounded-tr-xl">
-								<?php _e('Tất cả', 'bsc') ?>
-								<?php echo svg('arrow-right-tab') ?>
-							</a>
-						</li>
-						<li>
-							<a href="<?php echo $current_url ?>?s=<?php echo $search ?>&type_search=page"
-								class="<?php if ($type_search == 'page') echo 'active' ?> flex items-center justify-between w-full px-5 py-[15px] lg:text-lg font-semibold transition-all text-left hover:!text-white hover:!bg-primary-400 hover:!rounded-tr-xl hover:!rounded-br-xl [&:not(.active)]:text-black text-white [&:not(.active)]:bg-white bg-primary-400 [&:not(.active)]:rounded-br-none rounded-br-xl [&:not(.active)]:rounded-tr-none rounded-tr-xl">
-								<?php _e('Trang', 'bsc') ?>
-								<?php echo svg('arrow-right-tab') ?>
-							</a>
-						</li>
-						<li>
-							<a href="<?php echo $current_url ?>?s=<?php echo $search ?>&type_search=news"
-								class="<?php if ($type_search == 'news') echo 'active' ?> flex items-center justify-between w-full px-5 py-[15px] lg:text-lg font-semibold transition-all text-left hover:!text-white hover:!bg-primary-400 hover:!rounded-tr-xl hover:!rounded-br-xl [&:not(.active)]:text-black text-white [&:not(.active)]:bg-white bg-primary-400 [&:not(.active)]:rounded-br-none rounded-br-xl [&:not(.active)]:rounded-tr-none rounded-tr-xl">
-								<?php _e('Tin tức BSC', 'bsc') ?>
-								<?php echo svg('arrow-right-tab') ?>
-							</a>
-						</li>
-						<li>
-							<a href="<?php echo $current_url ?>?s=<?php echo $search ?>&type_search=cong_dong"
-								class="<?php if ($type_search == 'cong_dong') echo 'active' ?> flex items-center justify-between w-full px-5 py-[15px] lg:text-lg font-semibold transition-all text-left hover:!text-white hover:!bg-primary-400 hover:!rounded-tr-xl hover:!rounded-br-xl [&:not(.active)]:text-black text-white [&:not(.active)]:bg-white bg-primary-400 [&:not(.active)]:rounded-br-none rounded-br-xl [&:not(.active)]:rounded-tr-none rounded-tr-xl">
-								<?php _e('Trách nhiệm với cộng đồng', 'bsc') ?>
-								<?php echo svg('arrow-right-tab') ?>
-							</a>
-						</li>
-						<li>
-							<a href="<?php echo $current_url ?>?s=<?php echo $search ?>&type_search=khuyen_mai"
-								class="<?php if ($type_search == 'khuyen_mai') echo 'active' ?> flex items-center justify-between w-full px-5 py-[15px] lg:text-lg font-semibold transition-all text-left hover:!text-white hover:!bg-primary-400 hover:!rounded-tr-xl hover:!rounded-br-xl [&:not(.active)]:text-black text-white [&:not(.active)]:bg-white bg-primary-400 [&:not(.active)]:rounded-br-none rounded-br-xl [&:not(.active)]:rounded-tr-none rounded-tr-xl">
-								<?php _e('Chương trình khuyến mãi', 'bsc') ?>
-								<?php echo svg('arrow-right-tab') ?>
-							</a>
-						</li>
-						<li>
-							<a href="<?php echo $current_url ?>?s=<?php echo $search ?>&type_search=giao_dich"
-								class="<?php if ($type_search == 'giao_dich') echo 'active' ?> flex items-center justify-between w-full px-5 py-[15px] lg:text-lg font-semibold transition-all text-left hover:!text-white hover:!bg-primary-400 hover:!rounded-tr-xl hover:!rounded-br-xl [&:not(.active)]:text-black text-white [&:not(.active)]:bg-white bg-primary-400 [&:not(.active)]:rounded-br-none rounded-br-xl [&:not(.active)]:rounded-tr-none rounded-tr-xl">
-								<?php _e('Biểu phí giao dịch', 'bsc') ?>
-								<?php echo svg('arrow-right-tab') ?>
-							</a>
-						</li>
-						<li>
-							<a href="<?php echo $current_url ?>?s=<?php echo $search ?>&type_search=bao_cao"
-								class="<?php if ($type_search == 'bao_cao') echo 'active' ?> flex items-center justify-between w-full px-5 py-[15px] lg:text-lg font-semibold transition-all text-left hover:!text-white hover:!bg-primary-400 hover:!rounded-tr-xl hover:!rounded-br-xl [&:not(.active)]:text-black text-white [&:not(.active)]:bg-white bg-primary-400 [&:not(.active)]:rounded-br-none rounded-br-xl [&:not(.active)]:rounded-tr-none rounded-tr-xl">
-								<?php _e('Báo cáo phân tích', 'bsc') ?>
-								<?php echo svg('arrow-right-tab') ?>
-							</a>
-						</li>
-						<li>
-							<a href="<?php echo $current_url ?>?s=<?php echo $search ?>&type_search=kien_thuc"
-								class="<?php if ($type_search == 'kien_thuc') echo 'active' ?> flex items-center justify-between w-full px-5 py-[15px] lg:text-lg font-semibold transition-all text-left hover:!text-white hover:!bg-primary-400 hover:!rounded-tr-xl hover:!rounded-br-xl [&:not(.active)]:text-black text-white [&:not(.active)]:bg-white bg-primary-400 [&:not(.active)]:rounded-br-none rounded-br-xl [&:not(.active)]:rounded-tr-none rounded-tr-xl">
-								<?php _e('Kiến thức đầu tư', 'bsc') ?>
-								<?php echo svg('arrow-right-tab') ?>
-							</a>
-						</li>
-						<li>
-							<a href="<?php echo $current_url ?>?s=<?php echo $search ?>&type_search=co_dong"
-								class="<?php if ($type_search == 'co_dong') echo 'active' ?> flex items-center justify-between w-full px-5 py-[15px] lg:text-lg font-semibold transition-all text-left hover:!text-white hover:!bg-primary-400 hover:!rounded-tr-xl hover:!rounded-br-xl [&:not(.active)]:text-black text-white [&:not(.active)]:bg-white bg-primary-400 [&:not(.active)]:rounded-br-none rounded-br-xl [&:not(.active)]:rounded-tr-none rounded-tr-xl">
-								<?php _e('Quan hệ cổ đông', 'bsc') ?>
-								<?php echo svg('arrow-right-tab') ?>
-							</a>
-						</li>
-						<li>
-							<a href="<?php echo $current_url ?>?s=<?php echo $search ?>&type_search=so_tay"
-								class="<?php if ($type_search == 'so_tay') echo 'active' ?> flex items-center justify-between w-full px-5 py-[15px] lg:text-lg font-semibold transition-all text-left hover:!text-white hover:!bg-primary-400 hover:!rounded-tr-xl hover:!rounded-br-xl [&:not(.active)]:text-black text-white [&:not(.active)]:bg-white bg-primary-400 [&:not(.active)]:rounded-br-none rounded-br-xl [&:not(.active)]:rounded-tr-none rounded-tr-xl">
-								<?php _e('Sổ tay giao dịch', 'bsc') ?>
-								<?php echo svg('arrow-right-tab') ?>
-							</a>
-						</li>
-					</ul>
-				</div>
-				<div class="lg:flex-1">
+				<?php if ( !wp_is_mobile() && !bsc_is_mobile()) { ?> 
+					<div class="lg:w-[290px]">
+						<ul
+							class="flex flex-col py-[15px] pr-[15px] rounded-[15px] space-y-3 shadow-base sticky top-5 z-10">
+							<li>
+								<a href="<?php echo $current_url ?>?s=<?php echo $search ?>"
+									class="<?php if ($type_search == 'default') echo 'active' ?> flex items-center justify-between w-full px-5 py-[15px] lg:text-lg font-semibold transition-all text-left hover:!text-white hover:!bg-primary-400 hover:!rounded-tr-xl hover:!rounded-br-xl [&:not(.active)]:text-black text-white [&:not(.active)]:bg-white bg-primary-400 [&:not(.active)]:rounded-br-none rounded-br-xl [&:not(.active)]:rounded-tr-none rounded-tr-xl">
+									<?php _e('Tất cả', 'bsc') ?>
+									<?php echo svg('arrow-right-tab') ?>
+								</a>
+							</li>
+							<li>
+								<a href="<?php echo $current_url ?>?s=<?php echo $search ?>&type_search=page"
+									class="<?php if ($type_search == 'page') echo 'active' ?> flex items-center justify-between w-full px-5 py-[15px] lg:text-lg font-semibold transition-all text-left hover:!text-white hover:!bg-primary-400 hover:!rounded-tr-xl hover:!rounded-br-xl [&:not(.active)]:text-black text-white [&:not(.active)]:bg-white bg-primary-400 [&:not(.active)]:rounded-br-none rounded-br-xl [&:not(.active)]:rounded-tr-none rounded-tr-xl">
+									<?php _e('Trang', 'bsc') ?>
+									<?php echo svg('arrow-right-tab') ?>
+								</a>
+							</li>
+							<li>
+								<a href="<?php echo $current_url ?>?s=<?php echo $search ?>&type_search=news"
+									class="<?php if ($type_search == 'news') echo 'active' ?> flex items-center justify-between w-full px-5 py-[15px] lg:text-lg font-semibold transition-all text-left hover:!text-white hover:!bg-primary-400 hover:!rounded-tr-xl hover:!rounded-br-xl [&:not(.active)]:text-black text-white [&:not(.active)]:bg-white bg-primary-400 [&:not(.active)]:rounded-br-none rounded-br-xl [&:not(.active)]:rounded-tr-none rounded-tr-xl">
+									<?php _e('Tin tức BSC', 'bsc') ?>
+									<?php echo svg('arrow-right-tab') ?>
+								</a>
+							</li>
+							<li>
+								<a href="<?php echo $current_url ?>?s=<?php echo $search ?>&type_search=cong_dong"
+									class="<?php if ($type_search == 'cong_dong') echo 'active' ?> flex items-center justify-between w-full px-5 py-[15px] lg:text-lg font-semibold transition-all text-left hover:!text-white hover:!bg-primary-400 hover:!rounded-tr-xl hover:!rounded-br-xl [&:not(.active)]:text-black text-white [&:not(.active)]:bg-white bg-primary-400 [&:not(.active)]:rounded-br-none rounded-br-xl [&:not(.active)]:rounded-tr-none rounded-tr-xl">
+									<?php _e('Trách nhiệm với cộng đồng', 'bsc') ?>
+									<?php echo svg('arrow-right-tab') ?>
+								</a>
+							</li>
+							<li>
+								<a href="<?php echo $current_url ?>?s=<?php echo $search ?>&type_search=khuyen_mai"
+									class="<?php if ($type_search == 'khuyen_mai') echo 'active' ?> flex items-center justify-between w-full px-5 py-[15px] lg:text-lg font-semibold transition-all text-left hover:!text-white hover:!bg-primary-400 hover:!rounded-tr-xl hover:!rounded-br-xl [&:not(.active)]:text-black text-white [&:not(.active)]:bg-white bg-primary-400 [&:not(.active)]:rounded-br-none rounded-br-xl [&:not(.active)]:rounded-tr-none rounded-tr-xl">
+									<?php _e('Chương trình khuyến mãi', 'bsc') ?>
+									<?php echo svg('arrow-right-tab') ?>
+								</a>
+							</li>
+							<li>
+								<a href="<?php echo $current_url ?>?s=<?php echo $search ?>&type_search=giao_dich"
+									class="<?php if ($type_search == 'giao_dich') echo 'active' ?> flex items-center justify-between w-full px-5 py-[15px] lg:text-lg font-semibold transition-all text-left hover:!text-white hover:!bg-primary-400 hover:!rounded-tr-xl hover:!rounded-br-xl [&:not(.active)]:text-black text-white [&:not(.active)]:bg-white bg-primary-400 [&:not(.active)]:rounded-br-none rounded-br-xl [&:not(.active)]:rounded-tr-none rounded-tr-xl">
+									<?php _e('Biểu phí giao dịch', 'bsc') ?>
+									<?php echo svg('arrow-right-tab') ?>
+								</a>
+							</li>
+							<li>
+								<a href="<?php echo $current_url ?>?s=<?php echo $search ?>&type_search=bao_cao"
+									class="<?php if ($type_search == 'bao_cao') echo 'active' ?> flex items-center justify-between w-full px-5 py-[15px] lg:text-lg font-semibold transition-all text-left hover:!text-white hover:!bg-primary-400 hover:!rounded-tr-xl hover:!rounded-br-xl [&:not(.active)]:text-black text-white [&:not(.active)]:bg-white bg-primary-400 [&:not(.active)]:rounded-br-none rounded-br-xl [&:not(.active)]:rounded-tr-none rounded-tr-xl">
+									<?php _e('Báo cáo phân tích', 'bsc') ?>
+									<?php echo svg('arrow-right-tab') ?>
+								</a>
+							</li>
+							<li>
+								<a href="<?php echo $current_url ?>?s=<?php echo $search ?>&type_search=kien_thuc"
+									class="<?php if ($type_search == 'kien_thuc') echo 'active' ?> flex items-center justify-between w-full px-5 py-[15px] lg:text-lg font-semibold transition-all text-left hover:!text-white hover:!bg-primary-400 hover:!rounded-tr-xl hover:!rounded-br-xl [&:not(.active)]:text-black text-white [&:not(.active)]:bg-white bg-primary-400 [&:not(.active)]:rounded-br-none rounded-br-xl [&:not(.active)]:rounded-tr-none rounded-tr-xl">
+									<?php _e('Kiến thức đầu tư', 'bsc') ?>
+									<?php echo svg('arrow-right-tab') ?>
+								</a>
+							</li>
+							<li>
+								<a href="<?php echo $current_url ?>?s=<?php echo $search ?>&type_search=co_dong"
+									class="<?php if ($type_search == 'co_dong') echo 'active' ?> flex items-center justify-between w-full px-5 py-[15px] lg:text-lg font-semibold transition-all text-left hover:!text-white hover:!bg-primary-400 hover:!rounded-tr-xl hover:!rounded-br-xl [&:not(.active)]:text-black text-white [&:not(.active)]:bg-white bg-primary-400 [&:not(.active)]:rounded-br-none rounded-br-xl [&:not(.active)]:rounded-tr-none rounded-tr-xl">
+									<?php _e('Quan hệ cổ đông', 'bsc') ?>
+									<?php echo svg('arrow-right-tab') ?>
+								</a>
+							</li>
+							<li>
+								<a href="<?php echo $current_url ?>?s=<?php echo $search ?>&type_search=so_tay"
+									class="<?php if ($type_search == 'so_tay') echo 'active' ?> flex items-center justify-between w-full px-5 py-[15px] lg:text-lg font-semibold transition-all text-left hover:!text-white hover:!bg-primary-400 hover:!rounded-tr-xl hover:!rounded-br-xl [&:not(.active)]:text-black text-white [&:not(.active)]:bg-white bg-primary-400 [&:not(.active)]:rounded-br-none rounded-br-xl [&:not(.active)]:rounded-tr-none rounded-tr-xl">
+									<?php _e('Sổ tay giao dịch', 'bsc') ?>
+									<?php echo svg('arrow-right-tab') ?>
+								</a>
+							</li>
+						</ul>
+					</div>
+									
+				<?php } ?>
+				<div class="<?php echo !wp_is_mobile() && !bsc_is_mobile() ?'flex-1':'text-xs' ?>">
 					<?php
 					if (isset($_GET['posts_to_show'])) {
 						$post_per_page = $_GET['posts_to_show'];
@@ -506,7 +513,7 @@ $type_search =  $_GET['type_search'] ?: 'default';
 					}
 					$total_page = ceil($total_post / $post_per_page);
 					?>
-					<p class="mb-10 font-Helvetica">
+					<p class="font-Helvetica sm:text-base text-xs <?php echo !wp_is_mobile() && !bsc_is_mobile() ?'mb-10':'mb-6' ?>">
 						<?php _e('Tìm thấy', 'bsc') ?> <?php echo number_format($total_post) ?> <?php _e('kết quả cho từ khóa', 'bsc') ?>: <span
 							class="font-bold text-primary-300"><?php echo  $search ?></span>
 					</p>
@@ -516,8 +523,8 @@ $type_search =  $_GET['type_search'] ?: 'default';
 							foreach ($all_results as $result) :
 							?>
 								<li class="[&:not(:first-child)]:pt-6 pb-4 border-b border-[#C9CCD2]">
-									<div class="space-y-4">
-										<a href="<?php echo $result['permalink']; ?>" class="text-lg font-bold block">
+									<div class="<?php echo !wp_is_mobile() && !bsc_is_mobile() ?'space-y-4':'space-y-[12px]' ?>">
+										<a href="<?php echo $result['permalink']; ?>" class="font-bold block <?php echo !wp_is_mobile() && !bsc_is_mobile() ?'text-lg':'text-base' ?>">
 											<?php
 											if ($search == '') {
 												echo $result['title'];
