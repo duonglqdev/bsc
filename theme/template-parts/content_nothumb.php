fcffb5647f285849cfe927de718c716660c7aceb
duonglq.dev <duonglq.dev@gmail.com>
2024-12-25 09:21:59 +0700

    https://wework.base.vn/mytasks?task=5502747

--- theme/template-parts/content_nothumb.php
+++ theme/template-parts/content_nothumb.php
@@ -1,17 +1,30 @@
-<?php
-if ($args['data']) {
+<?php if ($args['data']) {
     $news = $args['data'];
     $date = $news->postdate;
     $date_parts = explode('T', $date);
     $day = $date_parts[0];
     $day_of_month = date('d', strtotime($day));
+    $day_of_year = date('Y', strtotime($day));
     setlocale(LC_TIME, 'vi_VN.UTF-8');
     if (get_locale() == 'vi') {
-        $weekday_number = date('N', strtotime($day));
-        $weekday_names = [__('Thứ', 'bsc') . ' 2', __('Thứ', 'bsc') . ' 3', __('Thứ', 'bsc') . ' 4', __('Thứ', 'bsc') . ' 5', __('Thứ', 'bsc') . ' 6', __('Thứ', 'bsc') . ' 7', __('Chủ Nhật', 'bsc')];
-        $weekday_name = $weekday_names[$weekday_number - 1];
+        $month_number = date('n', strtotime($day));
+        $month_names = [
+            __('Tháng', 'bsc') . ' 1',
+            __('Tháng', 'bsc') . ' 2',
+            __('Tháng', 'bsc') . ' 3',
+            __('Tháng', 'bsc') . ' 4',
+            __('Tháng', 'bsc') . ' 5',
+            __('Tháng', 'bsc') . ' 6',
+            __('Tháng', 'bsc') . ' 7',
+            __('Tháng', 'bsc') . ' 8',
+            __('Tháng', 'bsc') . ' 9',
+            __('Tháng', 'bsc') . ' 10',
+            __('Tháng', 'bsc') . ' 11',
+            __('Tháng', 'bsc') . ' 12',
+        ];
+        $month_name = $month_names[$month_number - 1];
     } else {
-        $weekday_name = date('l', strtotime($day));
+        $month_name = date('F', strtotime($day));
     }
 ?>
     <div
@@ -21,11 +34,14 @@ if ($args['data']) {
                 class="md:w-[100px] md:h-[100px] w-20 h-20 flex-col flex items-center justify-center rounded overflow-hidden shrink-0">
                 <p
                     class="date text-center bg-primary-300 text-white font-bold  py-[2px] px-1 leading-normal w-full <?php echo !wp_is_mobile() && !bsc_is_mobile() ?'text-xs':'text-xxs' ?>">
-                    <?php echo $weekday_name ?>
+                    <?php echo $month_name ?>
                 </p>
                 <p
-                    class="flex-1 flex flex-col justify-center items-center 2xl:text-2xl text-xl font-bold bg-primary-50 w-full">
-                    <?php echo $day_of_month ?>
+                    class="flex-1 flex flex-col justify-center items-center  font-bold bg-primary-50 w-full <?php echo !wp_is_mobile() && !bsc_is_mobile() ? '2xl:text-2xl text-xl' : 'text-xl' ?>">
+                    <?php echo $day_of_month; ?>
+                    <span class="text-primary-300 text-[12px] font-medium">
+                        <?php echo $day_of_year; ?>
+                    </span>
                 </p>
             </div>
             <div class=" <?php echo !wp_is_mobile() && !bsc_is_mobile() ?'ml-[30px]':'ml-4' ?>">
