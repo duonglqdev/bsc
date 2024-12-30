6c6585af49c79007c6977a5de0351075c2e19fbf
duonglq.dev <duonglq.dev@gmail.com>
2024-12-30 17:13:27 +0700

    https://wework.base.vn/mytasks?task=5491857

--- theme/components-stgd/note.php
+++ theme/components-stgd/note.php
@@ -1,6 +1,6 @@
 <?php if (get_sub_field('content')) { ?>
     <div
-        class="note shadow-base rounded-[10px] p-6 mt-4 font-Helvetica flex gap-2 max-w-[857px]">
+        class="note shadow-base rounded-[10px] lg:p-6 p-4 mt-4 font-Helvetica flex gap-2 max-w-[857px] lg:text-base text-sm">
         <?php if (get_sub_field('title')) { ?>
             <strong class="text-[#FF0017] shrink-0">
                 <?php the_sub_field('title') ?>
