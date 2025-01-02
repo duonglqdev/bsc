f2358f3765623c8ef1e81312f55c21d3872eea32
Gini Webseo <87066567+giniwebseo@users.noreply.github.com>
2025-01-02 22:06:55 +0700

    https://wework.base.vn/mytasks?task=5491857

---
+++ theme/components-stgd/video.php
@@ -0,0 +1,11 @@
+<div class="lg:my-6 my-4">
+	<a href="<?php echo check_link( get_sub_field( 'video_link' ) ) ?>"
+		class="block relative w-full pt-[52%] mb-6 overflow-hidden rounded-[10px] after:absolute after:inset-0 after:w-full after:h-full after:bg-[#000] after:bg-opacity-40"
+		data-fancybox="">
+		<?php echo wp_get_attachment_image( get_sub_field( 'image' ), 'large', '', array( 'class' => 'absolute w-full h-full inset-0 object-cover  transition-all duration-500 hover:scale-110' ) ) ?>
+		<div
+			class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 transition-all duration-500 hover:scale-110">
+			<?php echo svgClass( 'play', '', '', 'lg:w-14 lg:h-14 w-10 h-10' ) ?>
+		</div>
+	</a>
+</div>
