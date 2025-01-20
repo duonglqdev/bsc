84235d00c6c38fb31483452602c0320feb7f492d
Gini Webseo <87066567+giniwebseo@users.noreply.github.com>
2025-01-20 11:42:40 +0700

    Fix ý D

--- theme/components/list_khuyen_mai.php
+++ theme/components/list_khuyen_mai.php
@@ -19,7 +19,7 @@ if ( $cdctkm1_id_danh_muc ) {
 		'index' => $index
 	);
 	$response = get_data_with_cache( 'GetNews', $array_data, $time_cache );
-	if ( $response ) {
+	if ( $response && isset( $response->d ) && ! empty( $response->d ) ) {
 		if ( $response->totalrecord ) {
 			$total_post = $response->totalrecord;
 		} else {
@@ -27,28 +27,28 @@ if ( $cdctkm1_id_danh_muc ) {
 		}
 		$total_page = ceil( $total_post / $post_per_page );
 		?>
-				<section
-					class="<?php echo ! wp_is_mobile() && ! bsc_is_mobile() ? 'xl:my-[100px] my-20' : 'my-[50px]' ?> list_khuyen_mai"
-					<?php if ( get_sub_field( 'id_class' ) ) { ?> id="<?php echo get_sub_field( 'id_class' ) ?>" <?php } ?>>
-					<div class="container">
-						<div
-							class="grid <?php echo ! wp_is_mobile() && ! bsc_is_mobile() ? 'lg:grid-cols-3 md:grid-cols-2 gap-x-[21px] gap-y-[30px]' : 'md:grid-cols-2 grid-cols-1 gap-6' ?>">
-							<?php
-							foreach ( $response->d as $news ) {
-								get_template_part( 'template-parts/content', 'khuyen-mai', array(
-									'data' => $news,
-								) );
-							}
-							?>
-						</div>
-						<div class="pagination-center">
-							<?php get_template_part( 'components/pagination', '', array(
-								'get' => 'api',
-								'total_page' => $total_page,
-								'url' => get_permalink( get_the_ID() ),
-							) ) ?>
-						</div>
-					</div>
-				</section>
-		<?php }
+		<section
+			class="<?php echo ! wp_is_mobile() && ! bsc_is_mobile() ? 'xl:my-[100px] my-20' : 'my-[50px]' ?> list_khuyen_mai"
+			<?php if ( get_sub_field( 'id_class' ) ) { ?> id="<?php echo get_sub_field( 'id_class' ) ?>" <?php } ?>>
+			<div class="container">
+				<div
+					class="grid <?php echo ! wp_is_mobile() && ! bsc_is_mobile() ? 'lg:grid-cols-3 md:grid-cols-2 gap-x-[21px] gap-y-[30px]' : 'md:grid-cols-2 grid-cols-1 gap-6' ?>">
+					<?php
+					foreach ( $response->d as $news ) {
+						get_template_part( 'template-parts/content', 'khuyen-mai', array(
+							'data' => $news,
+						) );
+					}
+					?>
+				</div>
+				<div class="pagination-center">
+					<?php get_template_part( 'components/pagination', '', array(
+						'get' => 'api',
+						'total_page' => $total_page,
+						'url' => get_permalink( get_the_ID() ),
+					) ) ?>
+				</div>
+			</div>
+		</section>
+	<?php }
 } ?>
